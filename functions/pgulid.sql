3f808b6691a0323fb1c9d9b214223028b662ce4e
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-01-25 10:59:44 +0530

    feat: add postgrest_anon role

--- functions/pgulid.sql
+++ functions/pgulid.sql
@@ -15,9 +15,6 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
--- +goose Up
--- +goose StatementBegin
----
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
@@ -93,4 +90,3 @@ END
 $$
 LANGUAGE plpgsql
 VOLATILE;
--- +goose StatementEnd
