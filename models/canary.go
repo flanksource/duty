347e79692e9e611008c214ac35588e3412a0e3af
Aditya <thebeaditya@gmail.com>
2023-02-16 14:47:52 +0545

    struct migrations from canary-checker

---
+++ models/canary.go
@@ -0,0 +1,274 @@
+package models
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/flanksource/duty/types"
+	"github.com/google/uuid"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type Canary struct {
+	ID        uuid.UUID `gorm:"default:generate_ulid()"`
+	Spec      types.JSON
+	Labels    types.JSONStringMap
+	Source    string
+	Name      string
+	Namespace string
+	Checks    types.JSONStringMap `gorm:"-"`
+	Schedule  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `json:"deleted_at,omitempty" time_format:"postgres_timestamp"`
+}
+
+func (c Canary) GetCheckID(checkName string) string {
+	return c.Checks[checkName]
+}
+
+// NOTE: Commenting this out because this is conecerned with kubernetes spec
+//
+// func (c Canary) ToV1() *v1.Canary {
+// 	canary := v1.Canary{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name:      c.Name,
+// 			Namespace: c.Namespace,
+// 			Annotations: map[string]string{
+// 				"source": c.Source,
+// 			},
+// 			Labels: c.Labels,
+// 		},
+// 	}
+// 	var deletionTimestamp metav1.Time
+// 	if c.DeletedAt != nil && !c.DeletedAt.IsZero() {
+// 		deletionTimestamp = metav1.NewTime(*c.DeletedAt)
+// 		canary.ObjectMeta.DeletionTimestamp = &deletionTimestamp
+// 	}
+// 	if err := json.Unmarshal(c.Spec, &canary.Spec); err != nil {
+// 		logger.Debugf("Failed to unmarshal canary spec: %s", err)
+// 		return nil
+// 	}
+// 	id := c.ID.String()
+// 	canary.Status.PersistedID = &id
+// 	canary.Status.Checks = c.Checks
+// 	return &canary
+// }
+
+// func CanaryFromV1(canary v1.Canary) (Canary, error) {
+// 	spec, err := json.Marshal(canary.Spec)
+// 	if err != nil {
+// 		return Canary{}, err
+// 	}
+// 	var checks = make(map[string]string)
+// 	if canary.Status.Checks != nil {
+// 		checks = canary.Status.Checks
+// 	}
+// 	return Canary{
+// 		Spec:      spec,
+// 		Labels:    types.JSONStringMap(canary.Labels),
+// 		Name:      canary.Name,
+// 		Namespace: canary.Namespace,
+// 		Source:    canary.Annotations["source"],
+// 		Schedule:  canary.Spec.GetSchedule(),
+// 		Checks:    types.JSONStringMap(checks),
+// 	}, nil
+// }
+
+type ResultMode string
+
+// CanarySpec defines the desired state of Canary
+type CanarySpec struct {
+	Env            map[string]VarSource  `yaml:"env,omitempty" json:"env,omitempty"`
+	HTTP           []HTTPCheck           `yaml:"http,omitempty" json:"http,omitempty"`
+	DNS            []DNSCheck            `yaml:"dns,omitempty" json:"dns,omitempty"`
+	DockerPull     []DockerPullCheck     `yaml:"docker,omitempty" json:"docker,omitempty"`
+	DockerPush     []DockerPushCheck     `yaml:"dockerPush,omitempty" json:"dockerPush,omitempty"`
+	ContainerdPull []ContainerdPullCheck `yaml:"containerd,omitempty" json:"containerd,omitempty"`
+	ContainerdPush []ContainerdPushCheck `yaml:"containerdPush,omitempty" json:"containerdPush,omitempty"`
+	S3             []S3Check             `yaml:"s3,omitempty" json:"s3,omitempty"`
+	TCP            []TCPCheck            `yaml:"tcp,omitempty" json:"tcp,omitempty"`
+	Pod            []PodCheck            `yaml:"pod,omitempty" json:"pod,omitempty"`
+	LDAP           []LDAPCheck           `yaml:"ldap,omitempty" json:"ldap,omitempty"`
+	ICMP           []ICMPCheck           `yaml:"icmp,omitempty" json:"icmp,omitempty"`
+	Postgres       []PostgresCheck       `yaml:"postgres,omitempty" json:"postgres,omitempty"`
+	Mssql          []MssqlCheck          `yaml:"mssql,omitempty" json:"mssql,omitempty"`
+	Mysql          []MysqlCheck          `yaml:"mysql,omitempty" json:"mysql,omitempty"`
+	Restic         []ResticCheck         `yaml:"restic,omitempty" json:"restic,omitempty"`
+	Jmeter         []JmeterCheck         `yaml:"jmeter,omitempty" json:"jmeter,omitempty"`
+	Junit          []JunitCheck          `yaml:"junit,omitempty" json:"junit,omitempty"`
+	Helm           []HelmCheck           `yaml:"helm,omitempty" json:"helm,omitempty"`
+	Namespace      []NamespaceCheck      `yaml:"namespace,omitempty" json:"namespace,omitempty"`
+	Redis          []RedisCheck          `yaml:"redis,omitempty" json:"redis,omitempty"`
+	EC2            []EC2Check            `yaml:"ec2,omitempty" json:"ec2,omitempty"`
+	Prometheus     []PrometheusCheck     `yaml:"prometheus,omitempty" json:"prometheus,omitempty"`
+	MongoDB        []MongoDBCheck        `yaml:"mongodb,omitempty" json:"mongodb,omitempty"`
+	CloudWatch     []CloudWatchCheck     `yaml:"cloudwatch,omitempty" json:"cloudwatch,omitempty"`
+	GitHub         []GitHubCheck         `yaml:"github,omitempty" json:"github,omitempty"`
+	Kubernetes     []KubernetesCheck     `yaml:"kubernetes,omitempty" json:"kubernetes,omitempty"`
+	Folder         []FolderCheck         `yaml:"folder,omitempty" json:"folder,omitempty"`
+	Exec           []ExecCheck           `yaml:"exec,omitempty" json:"exec,omitempty"`
+	AwsConfig      []AwsConfigCheck      `yaml:"awsConfig,omitempty" json:"awsConfig,omitempty"`
+	AwsConfigRule  []AwsConfigRuleCheck  `yaml:"awsConfigRule,omitempty" json:"awsConfigRule,omitempty"`
+	DatabaseBackup []DatabaseBackupCheck `yaml:"databaseBackup,omitempty" json:"databaseBackup,omitempty"`
+	ConfigDB       []ConfigDBCheck       `yaml:"configDB,omitempty" json:"configDB,omitempty"`
+	Elasticsearch  []ElasticsearchCheck  `yaml:"elasticsearch,omitempty" json:"elasticsearch,omitempty"`
+	// interval (in seconds) to run checks on Deprecated in favor of Schedule
+	Interval uint64 `yaml:"interval,omitempty" json:"interval,omitempty"`
+	// Schedule to run checks on. Supports all cron expression, example: '30 3-6,20-23 * * *'. For more info about cron expression syntax see https://en.wikipedia.org/wiki/Cron
+	//  Also supports golang duration, can be set as '@every 1m30s' which runs the check every 1 minute and 30 seconds.
+	Schedule   string     `yaml:"schedule,omitempty" json:"schedule,omitempty"`
+	Icon       string     `yaml:"icon,omitempty" json:"icon,omitempty"`
+	Severity   string     `yaml:"severity,omitempty" json:"severity,omitempty"`
+	Owner      string     `yaml:"owner,omitempty" json:"owner,omitempty"`
+	ResultMode ResultMode `yaml:"resultMode,omitempty" json:"resultMode,omitempty"`
+}
+
+func (spec CanarySpec) GetAllChecks() []ICheck {
+	var checks []ICheck
+	for _, check := range spec.HTTP {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.DNS {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.DockerPull {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.DockerPush {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.ContainerdPull {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.ContainerdPush {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.S3 {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.TCP {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Pod {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.LDAP {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Postgres {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Mysql {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Mssql {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Redis {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Restic {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.ICMP {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Helm {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Namespace {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Jmeter {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Junit {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.EC2 {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Prometheus {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.MongoDB {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.CloudWatch {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.GitHub {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Kubernetes {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Folder {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Exec {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.AwsConfig {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.AwsConfigRule {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.DatabaseBackup {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.ConfigDB {
+		checks = append(checks, check)
+	}
+	for _, check := range spec.Elasticsearch {
+		checks = append(checks, check)
+	}
+	return checks
+}
+
+func (spec CanarySpec) GetSchedule() string {
+	if spec.Schedule != "" {
+		return spec.Schedule
+	}
+	if spec.Interval > 0 {
+		return fmt.Sprintf("@every %ds", spec.Interval)
+	}
+	return "@never"
+}
+
+type CanaryStatusCondition string
+
+var (
+	Passed  CanaryStatusCondition = "Passed"
+	Failed  CanaryStatusCondition = "Failed"
+	Invalid CanaryStatusCondition = "Invalid"
+)
+
+// CanaryStatus defines the observed state of Canary
+type CanaryStatus struct {
+	PersistedID *string `json:"persistedID,omitempty"`
+	// +optional
+	LastTransitionedTime *metav1.Time `json:"lastTransitionedTime,omitempty"`
+	// +optional
+	LastCheck *metav1.Time `json:"lastCheck,omitempty"`
+	// +optional
+	Message *string `json:"message,omitempty"`
+	// +optional
+	ErrorMessage *string `json:"errorMessage,omitempty"`
+	// +optional
+	Status *CanaryStatusCondition `json:"status,omitempty"`
+	// contains the name and id of the checks associated with the canary
+	Checks map[string]string `json:"checks,omitempty"`
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty" protobuf:"varint,3,opt,name=observedGeneration"`
+	// +optional
+	ChecksStatus map[string]*CheckStatus `json:"checkStatus,omitempty"`
+	// Availibility over a rolling 1h period
+	Uptime1H string `json:"uptime1h,omitempty"`
+	// Average latency to complete all checks
+	Latency1H string `json:"latency1h,omitempty"`
+	// used for keeping history of the checks
+	runnerName string `json:"-"`
+}
