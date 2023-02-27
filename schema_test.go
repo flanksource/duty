10cb3c82e5af08445d4cbe1c787d0528cf8e3955
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-02-27 10:57:28 +0530

    chore: refactor test files

--- schema_test.go
+++ schema_test.go
@@ -1,77 +1,24 @@
 package duty
 
 import (
-	"database/sql"
-	"io"
-	"testing"
-
-	. "github.com/fergusstrange/embedded-postgres"
 	"github.com/flanksource/commons/logger"
-	_ "github.com/flanksource/duty/types"
 	ginkgo "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-func TestSchema(t *testing.T) {
-	RegisterFailHandler(ginkgo.Fail)
-	ginkgo.RunSpecs(t, "Schema Suite")
-}
-
-var postgres *EmbeddedPostgres
-
-const pgUrl = "postgres://postgres:postgres@localhost:9876/test?sslmode=disable"
-
-func MustDB() *sql.DB {
-	db, err := NewDB(pgUrl)
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
-
-var _ = ginkgo.BeforeSuite(func() {
-	postgres = NewDatabase(DefaultConfig().
-		Database("test").
-		Port(9876).
-		Logger(io.Discard))
-	if err := postgres.Start(); err != nil {
-		ginkgo.Fail(err.Error())
-	}
-	logger.Infof("Started postgres on port 9876")
-	if pool != nil {
-		return
-	}
-	if _, err := NewPgxPool(pgUrl); err != nil {
-		ginkgo.Fail(err.Error())
-	}
-	if _, err := NewDB(pgUrl); err != nil {
-		ginkgo.Fail(err.Error())
-	}
-	err := Migrate(pgUrl)
-	Expect(err).ToNot(HaveOccurred())
-
-})
-
-var _ = ginkgo.AfterSuite(func() {
-	logger.Infof("Stopping postgres")
-	if err := postgres.Stop(); err != nil {
-		ginkgo.Fail(err.Error())
-	}
-})
-
 var _ = ginkgo.Describe("Schema", func() {
 	ginkgo.It("should be able to run migrations", func() {
 		logger.Infof("Running migrations against %s", pgUrl)
-		// run again to ensure idempotency
+		// run migrations again to ensure idempotency
 		err := Migrate(pgUrl)
 		Expect(err).ToNot(HaveOccurred())
 	})
-	ginkgo.It(" Gorm Can connect", func() {
-		gorm, err := NewGorm(pgUrl, DefaultGormConfig())
+	ginkgo.It("Gorm can connect", func() {
+		gormDB, err := NewGorm(pgUrl, DefaultGormConfig())
 		Expect(err).ToNot(HaveOccurred())
 		var people int64
-		Expect(gorm.Table("people").Count(&people).Error).ToNot(HaveOccurred())
-		Expect(people).To(Equal(int64(1)))
+		Expect(gormDB.Table("people").Count(&people).Error).ToNot(HaveOccurred())
+		Expect(people).NotTo(BeZero())
 	})
 })
 
---
+++ suite_test.go
@@ -0,0 +1,145 @@
+package duty
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"testing"
+
+	. "github.com/fergusstrange/embedded-postgres"
+	"github.com/flanksource/commons/logger"
+	"github.com/flanksource/duty/fixtures/dummy"
+	"github.com/flanksource/duty/models"
+	_ "github.com/flanksource/duty/types"
+	ginkgo "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
+)
+
+func TestSchema(t *testing.T) {
+	RegisterFailHandler(ginkgo.Fail)
+	ginkgo.RunSpecs(t, "Duty Suite")
+}
+
+var postgresServer *EmbeddedPostgres
+
+const pgUrl = "postgres://postgres:postgres@localhost:9876/test?sslmode=disable"
+
+var testDB *gorm.DB
+
+func MustDB() *sql.DB {
+	db, err := NewDB(pgUrl)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+var _ = ginkgo.BeforeSuite(func() {
+	postgresServer = NewDatabase(DefaultConfig().
+		Database("test").
+		Port(9876).
+		Logger(io.Discard))
+	if err := postgresServer.Start(); err != nil {
+		ginkgo.Fail(err.Error())
+	}
+	logger.Infof("Started postgres on port 9876")
+	if pool != nil {
+		return
+	}
+	if _, err := NewPgxPool(pgUrl); err != nil {
+		ginkgo.Fail(err.Error())
+	}
+	if _, err := NewDB(pgUrl); err != nil {
+		ginkgo.Fail(err.Error())
+	}
+	err := Migrate(pgUrl)
+	Expect(err).ToNot(HaveOccurred())
+
+	testDB, err = NewGorm(pgUrl, DefaultGormConfig())
+	Expect(err).ToNot(HaveOccurred())
+
+	populateDBWithDummyModels()
+})
+
+var _ = ginkgo.AfterSuite(func() {
+	logger.Infof("Stopping postgres")
+	if err := postgresServer.Stop(); err != nil {
+		ginkgo.Fail(err.Error())
+	}
+})
+
+func populateDBWithDummyModels() {
+	var err error
+	createTime := dummy.DummyCreatedAt
+	for _, c := range dummy.AllDummyPeople {
+		err = testDB.Create(&c).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, c := range dummy.AllDummyComponents {
+		c.UpdatedAt = models.LocalTime(createTime)
+		err = testDB.Create(&c).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, c := range dummy.AllDummyComponentRelationships {
+		c.UpdatedAt = createTime
+		err = testDB.Create(&c).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, c := range dummy.AllDummyConfigs {
+		c.CreatedAt = createTime
+		err = testDB.Create(&c).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, c := range dummy.AllDummyConfigAnalysis {
+		c.FirstObserved = &createTime
+		err = testDB.Create(&c).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, c := range dummy.AllDummyConfigComponentRelationships {
+		err = testDB.Create(&c).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, c := range dummy.AllDummyIncidents {
+		err = testDB.Create(&c).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, c := range dummy.AllDummyHypotheses {
+		err = testDB.Create(&c).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, c := range dummy.AllDummyEvidences {
+		err = testDB.Create(&c).Error
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	logger.Infof("Populated database with dummy models")
+}
+
+func readTestFile(path string) string {
+	d, err := ioutil.ReadFile(path)
+	// We panic here because text fixtures should always be readable
+	if err != nil {
+		panic(fmt.Errorf("Unable to read file:%s due to %v", path, err))
+	}
+	return string(d)
+}
