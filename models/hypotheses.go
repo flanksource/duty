e68e566a00fa82294de9d2931d7fc6f3a485fde3
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-02-27 09:28:56 +0530

    feat: add models for people, evidences and hypothesis

---
+++ models/hypotheses.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Hypothesis struct {
+	ID         uuid.UUID  `json:"id,omitempty" gorm:"default:generate_ulid()"`
+	IncidentID uuid.UUID  `json:"incident_id,omitempty"`
+	Type       string     `json:"type,omitempty"`
+	Title      string     `json:"title,omitempty"`
+	Status     string     `json:"status,omitempty"`
+	ParentID   *uuid.UUID `json:"parent_id,omitempty"`
+	TeamID     *uuid.UUID `json:"team_id,omitempty"`
+	Owner      *uuid.UUID `json:"owner,omitempty"`
+	CreatedAt  *time.Time `json:"created_at,omitempty"`
+	UpdatedAt  *time.Time `json:"updated_at,omitempty"`
+	CreatedBy  uuid.UUID  `json:"created_by,omitempty"`
+}
+
+func (Hypothesis) TableName() string {
+	return "hypotheses"
+}
