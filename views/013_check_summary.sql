52b0c9558d837779d83ed7cf823b1d453b70fdff
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-03-03 10:27:49 +0530

    feat: add transition time for checks

---
+++ views/013_check_summary.sql
@@ -0,0 +1,31 @@
+-- Materialized view for check status summary
+CREATE MATERIALIZED VIEW IF NOT EXISTS check_status_summary AS
+    SELECT
+        check_id,
+        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY duration) as p99,
+        COUNT(*) FILTER ( WHERE status = TRUE) as passed,
+        COUNT(*) FILTER ( WHERE status = FALSE) as failed,
+        MAX(time) as last_check,
+        MAX(time) FILTER (WHERE status = TRUE) as last_pass,
+        MAX(time) FILTER (WHERE status = FALSE) as last_fail
+    FROM
+        check_statuses
+    WHERE
+        time > (NOW() at TIME ZONE 'utc' - Interval '1 hour')
+    GROUP BY check_id;
+
+-- For last transition
+CREATE OR REPLACE FUNCTION update_last_transition_time_for_check()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE checks SET last_transition_time = NOW() WHERE id = NEW.id;
+    RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER checks_last_transition_time
+    AFTER UPDATE
+    ON checks
+    FOR EACH ROW
+    WHEN (OLD.status IS DISTINCT FROM NEW.status)
+    EXECUTE PROCEDURE update_last_transition_time_for_check();
