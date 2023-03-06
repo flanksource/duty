e2f053b5d4e28d1c9615eeac35de53462f1b99e0
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-03-06 21:31:26 +0530

    chore: add tests and fixtures for check summary

--- fixtures/dummy/canaries.go
+++ fixtures/dummy/canaries.go
@@ -6,7 +6,7 @@ import (
 )
 
 var LogisticsAPICanary = models.Canary{
-	ID:        uuid.New(),
+	ID:        uuid.MustParse("0186b7a5-a2a4-86fd-c326-3a2104a2777f"),
 	Name:      "dummy-logistics-api-canary",
 	Namespace: "logistics",
 	Spec:      []byte("{}"),
@@ -14,7 +14,7 @@ var LogisticsAPICanary = models.Canary{
 }
 
 var LogisticsDBCanary = models.Canary{
-	ID:        uuid.New(),
+	ID:        uuid.MustParse("0186b7a5-f246-3628-0d68-30bffc13244d"),
 	Name:      "dummy-logistics-db-canary",
 	Namespace: "logistics",
 	Spec:      []byte("{}"),
