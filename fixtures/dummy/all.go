534cca1012b0d49f5e7a4ac1cc86563f0d60b718
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-03-06 11:37:58 +0530

    feat: api to delete dummy fixtures

--- fixtures/dummy/all.go
+++ fixtures/dummy/all.go
@@ -68,3 +68,72 @@ func PopulateDBWithDummyModels(gormDB *gorm.DB) error {
 	}
 	return nil
 }
+
+func DeleteDummyModelsFromDB(gormDB *gorm.DB) error {
+	var err error
+
+	if gormDB == nil {
+		panic("yaa")
+	}
+	if err = gormDB.Exec(`DELETE FROM incident_histories`).Error; err != nil {
+		return err
+	}
+
+	for _, c := range AllDummyEvidences {
+		err = gormDB.Delete(&c).Error
+		if err != nil {
+			return err
+		}
+	}
+	for _, c := range AllDummyHypotheses {
+		err = gormDB.Delete(&c).Error
+		if err != nil {
+			return err
+		}
+	}
+	for _, c := range AllDummyIncidents {
+		err = gormDB.Delete(&c).Error
+		if err != nil {
+			return err
+		}
+	}
+	for _, c := range AllDummyConfigComponentRelationships {
+		err = gormDB.Where("component_id = ?", c.ComponentID).Delete(&c).Error
+		if err != nil {
+			return err
+		}
+	}
+	for _, c := range AllDummyConfigAnalysis {
+		err = gormDB.Delete(&c).Error
+		if err != nil {
+			return err
+		}
+	}
+	for _, c := range AllDummyConfigs {
+		err = gormDB.Delete(&c).Error
+		if err != nil {
+			return err
+		}
+	}
+	for _, c := range AllDummyComponentRelationships {
+		err = gormDB.Where("component_id = ?", c.ComponentID).Delete(&c).Error
+		if err != nil {
+			return err
+		}
+	}
+	for i := range AllDummyComponents {
+		// We need to delete in reverse order
+		elem := AllDummyComponents[len(AllDummyComponents)-1-i]
+		err = gormDB.Delete(&elem).Error
+		if err != nil {
+			return err
+		}
+	}
+	for _, c := range AllDummyPeople {
+		err = gormDB.Delete(&c).Error
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
