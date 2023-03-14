28e6ffa2066a625f9ee9af5f301b2526aa3bf449
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-03-14 17:55:53 +0530

    chore: use configs as a base for config_item_aws query

--- views/014_config_item_by_type.sql
+++ views/014_config_item_by_type.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW config_items_aws AS
-    SELECT *, tags->>'zone' as zone, tags->>'region' as region
-    FROM config_items
+    SELECT *, tags->>'zone' AS zone, tags->>'region' AS region, tags->>'account' AS account
+    FROM configs
     WHERE external_type LIKE 'AWS::%';
