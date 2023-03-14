6858296de0c4e412333e43c2e2f8bafd9da791fd
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-03-14 12:32:19 +0530

    fix: use deleted_at in select query

--- views/003_analysis_views.sql
+++ views/003_analysis_views.sql
@@ -25,7 +25,8 @@ CREATE OR REPLACE VIEW analysis_by_component AS
          config_analysis.first_observed,config_analysis.last_observed,config_analysis.created_by,config_analysis.id as analysis_id, config_analysis.severity, component_id
   FROM config_analysis
   INNER JOIN config_component_relationships relations on relations.config_id = config_analysis.config_id
-  INNER JOIN config_items configs on configs.id = config_analysis.config_id;
+  INNER JOIN config_items configs on configs.id = config_analysis.config_id
+  WHERE configs.deleted_at IS NULL;
 
 -- analysis_summary_by_component
 DROP VIEW IF EXISTS analysis_summary_by_component;
@@ -36,7 +37,10 @@ CREATE OR REPLACE VIEW analysis_summary_by_component AS
             SELECT config_component_relationships.component_id AS id, config_analysis.analysis_type AS type, json_build_object(severity, count(*)) AS severity_agg
             FROM config_analysis
             LEFT JOIN config_component_relationships ON config_analysis.config_id = config_component_relationships.config_id
-            WHERE config_component_relationships.deleted_at IS NULL
+            INNER JOIN config_items configs ON configs.id = config_analysis.config_id
+            WHERE
+                config_component_relationships.deleted_at IS NULL AND
+                configs.deleted_at IS NULL
             GROUP BY config_analysis.severity, config_analysis.analysis_type, config_component_relationships.component_id
         )
         AS summary, json_each(summary.severity_agg) AS f(k,v) GROUP BY summary.type, summary.id
