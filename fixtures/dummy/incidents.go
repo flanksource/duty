ad78c8998a184ea32757141f996d5ba0bbb1a823
Aditya <thebeaditya@gmail.com>
2023-04-17 17:01:45 +0545

    added tests for incident summary view

--- fixtures/dummy/incidents.go
+++ fixtures/dummy/incidents.go
@@ -1,17 +1,43 @@
 package dummy
 
 import (
+	"time"
+
 	"github.com/flanksource/duty/models"
 	"github.com/google/uuid"
 )
 
 var LogisticsAPIDownIncident = models.Incident{
-	ID:        uuid.New(),
-	Title:     "Logistics API is down",
-	CreatedBy: JohnDoe.ID,
-	Type:      models.IncidentTypeAvailability,
-	Status:    models.IncidentStatusOpen,
-	Severity:  "Blocker",
+	ID:          uuid.MustParse("7c05a739-8a1c-4999-85f7-d93d03f32044"),
+	Title:       "Logistics API is down",
+	CreatedBy:   JohnDoe.ID,
+	Type:        models.IncidentTypeAvailability,
+	Status:      models.IncidentStatusOpen,
+	Severity:    "Blocker",
+	CommanderID: &JohnDoe.ID,
 }
 
 var AllDummyIncidents = []models.Incident{LogisticsAPIDownIncident}
+
+var FirstComment = models.Comment{
+	ID:         uuid.New(),
+	CreatedBy:  JohnWick.ID,
+	Comment:    "This is a comment",
+	IncidentID: LogisticsAPIDownIncident.ID,
+	CreatedAt:  time.Now(),
+	UpdatedAt:  time.Now(),
+}
+
+var AllDummyComments = []models.Comment{FirstComment}
+
+var FirstResponder = models.Responder{
+	ID:         uuid.New(),
+	IncidentID: LogisticsAPIDownIncident.ID,
+	Type:       "whattype",
+	PersonID:   &JohnWick.ID,
+	CreatedBy:  JohnWick.ID,
+	CreatedAt:  time.Now(),
+	UpdatedAt:  time.Now(),
+}
+
+var AllDummyResponders = []models.Responder{FirstResponder}
