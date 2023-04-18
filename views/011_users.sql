141356fb3ef93c07d2df0451fe5a0725bdc3c5ed
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-04-18 08:44:31 +0530

    feat: add viewer role when user is created

--- views/011_users.sql
+++ views/011_users.sql
@@ -4,6 +4,11 @@ RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO people (id, name, email)
     VALUES (NEW.id, concat(NEW.traits::json->'name'->>'first', ' ', NEW.traits::json->'name'->>'last'), NEW.traits::json->>'email');
+
+    -- Give any new user viewer role
+    INSERT INTO casbin_rule (ptype, v0, v1)
+    VALUES ('g', NEW.id::text, 'viewer');
+
     RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
