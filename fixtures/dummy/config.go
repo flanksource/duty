cf1aa67c8e4bad8a2eb905f9da4b76db2d16c2af
Aditya <thebeaditya@gmail.com>
2023-04-19 10:19:02 +0545

    added tests for config_type_summary

--- fixtures/dummy/config.go
+++ fixtures/dummy/config.go
@@ -6,43 +6,55 @@ import (
 )
 
 var EKSCluster = models.ConfigItem{
-	ID: uuid.New(),
+	ID:         uuid.New(),
+	ConfigType: "kubernetesCluster",
 }
 
 var KubernetesCluster = models.ConfigItem{
-	ID: uuid.New(),
+	ID:         uuid.New(),
+	ConfigType: "kubernetesCluster",
 }
 
 var KubernetesNodeA = models.ConfigItem{
-	ID: uuid.New(),
+	ID:           uuid.New(),
+	ConfigType:   "kubernetesNode",
+	CostTotal30d: 1,
 }
 
 var KubernetesNodeB = models.ConfigItem{
-	ID: uuid.New(),
+	ID:           uuid.New(),
+	ConfigType:   "kubernetesNode",
+	CostTotal30d: 1.5,
 }
 
 var EC2InstanceA = models.ConfigItem{
-	ID: uuid.New(),
+	ID:         uuid.New(),
+	ConfigType: "ec2",
 }
 
 var EC2InstanceB = models.ConfigItem{
-	ID: uuid.New(),
+	ID:         uuid.New(),
+	ConfigType: "ec2",
 }
 
 var LogisticsAPIDeployment = models.ConfigItem{
-	ID: uuid.New(),
+	ID:         uuid.New(),
+	ConfigType: "deployment",
 }
 
 var LogisticsUIDeployment = models.ConfigItem{
-	ID: uuid.New(),
+	ID:         uuid.New(),
+	ConfigType: "deployment",
 }
 
 var LogisticsWorkerDeployment = models.ConfigItem{
-	ID: uuid.New(),
+	ID:         uuid.New(),
+	ConfigType: "deployment",
 }
 
 var LogisticsDBRDS = models.ConfigItem{
-	ID: uuid.New(),
+	ID:         uuid.New(),
+	ConfigType: "database",
 }
 
 var AllDummyConfigs = []models.ConfigItem{
