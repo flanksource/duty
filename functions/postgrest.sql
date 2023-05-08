4321bc4699c2c1c290f2b2e4105b7f0661a95fc0
Yash Mehrotra <yashmehrotra95@gmail.com>
2023-05-08 12:36:30 +0530

    fix: allow postgrest to select sequences

--- functions/postgrest.sql
+++ functions/postgrest.sql
@@ -4,6 +4,7 @@ BEGIN
         CREATE ROLE postgrest_api;
         GRANT SELECT, UPDATE, DELETE, INSERT ON ALL TABLES IN SCHEMA public TO postgrest_api;
         ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, UPDATE, DELETE, INSERT ON TABLES TO postgrest_api;
+        GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO postgrest_api;
     END IF;
 
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'postgrest_anon') THEN
