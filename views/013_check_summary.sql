a864de6911ed6f81edf6d836a8fbc0212af83341
Aditya Thebe <thebeaditya@gmail.com>
2023-07-18 18:00:56 +0545

    feat: added check summary for component func

--- views/013_check_summary.sql
+++ views/013_check_summary.sql
@@ -44,3 +44,61 @@ CREATE
 OR REPLACE TRIGGER checks_last_transition_time BEFORE
 UPDATE ON checks FOR EACH ROW WHEN (OLD.status IS DISTINCT FROM NEW.status)
 EXECUTE PROCEDURE update_last_transition_time_for_check ();
+
+-- check summary view
+CREATE OR REPLACE VIEW check_summary AS
+  WITH check_component_relationship_by_check AS (
+  SELECT
+    check_id,
+    json_agg(component_id) AS components
+  FROM
+    check_component_relationships
+  GROUP BY
+    check_id
+  )
+  SELECT
+    checks.id,
+    checks.canary_id,
+    json_build_object(
+      'passed', check_status_summary.passed,
+      'failed', check_status_summary.failed,
+      'last_pass', check_status_summary.last_pass,
+      'last_fail', check_status_summary.last_fail
+    ) AS uptime,
+    json_build_object('p99', check_status_summary.p99) AS latency,
+    checks.last_transition_time,
+    checks.type,
+    checks.icon,
+    checks.name,
+    checks.status,
+    checks.description,
+    canaries.namespace,
+    canaries.name as canary_name,
+    canaries.labels,
+    checks.severity,
+    checks.owner,
+    checks.last_runtime,
+    checks.created_at,
+    checks.updated_at,
+    checks.deleted_at,
+    checks.silenced_at,
+    check_component_relationship_by_check.components
+  FROM
+    checks
+    LEFT JOIN check_component_relationship_by_check ON checks.id = check_component_relationship_by_check.check_id
+    INNER JOIN canaries ON checks.canary_id = canaries.id
+    INNER JOIN check_status_summary ON checks.id = check_status_summary.check_id;
+
+-- Check summary by component
+DROP FUNCTION IF EXISTS check_summary_for_component;
+
+CREATE OR REPLACE FUNCTION check_summary_for_component(id uuid) RETURNS setof check_summary
+AS $$
+  BEGIN
+    RETURN QUERY
+    SELECT check_summary.* FROM check_summary
+    INNER JOIN check_component_relationships
+      ON check_component_relationships.check_id = check_summary.id 
+    WHERE check_component_relationships.component_id = $1;
+  END;
+$$ language plpgsql;
