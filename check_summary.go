21e1fb64007cc088294b4cb5789705dfe34ab244
Aditya Thebe <thebeaditya@gmail.com>
2023-07-24 12:03:24 +0545

    feat: added check summary for component func (#221)
    * feat: added check summary for component func
    
    * chore: remove component ids from check summary
    
    * chore: use check summary view instead. fixed tests.

--- check_summary.go
+++ check_summary.go
@@ -10,57 +10,13 @@ import (
 )
 
 func QueryCheckSummary(ctx context.Context, dbpool *pgxpool.Pool) (models.Checks, error) {
-	query := `
-    SELECT json_agg(result)
-    FROM (
-        WITH check_component_relationship_by_check AS (
-            SELECT
-                check_id,
-                json_agg(component_id) AS components
-            FROM
-                check_component_relationships
-            GROUP BY
-                check_id
-        )
-        SELECT
-            checks.id::text,
-            checks.canary_id::text,
-            json_build_object(
-                'passed', check_status_summary.passed,
-                'failed', check_status_summary.failed,
-                'last_pass', check_status_summary.last_pass,
-                'last_fail', check_status_summary.last_fail
-            ) AS uptime,
-            json_build_object('p99', check_status_summary.p99) AS latency,
-            checks.last_transition_time,
-            checks.type,
-            checks.icon,
-            checks.name,
-            checks.status,
-            checks.description,
-            canaries.namespace,
-            canaries.name as canary_name,
-            canaries.labels,
-            checks.severity,
-            checks.owner,
-            checks.last_runtime,
-            checks.created_at,
-            checks.updated_at,
-            checks.deleted_at,
-            checks.silenced_at,
-            check_component_relationship_by_check.components
-        FROM
-            checks
-            LEFT JOIN check_component_relationship_by_check ON checks.id = check_component_relationship_by_check.check_id
-            INNER JOIN canaries ON checks.canary_id = canaries.id
-            INNER JOIN check_status_summary ON checks.id = check_status_summary.check_id
-    ) AS result
-    `
 	if _, ok := ctx.Deadline(); !ok {
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithTimeout(ctx, DefaultQueryTimeout)
 		defer cancel()
 	}
+
+	query := `SELECT json_agg(result) FROM check_summary AS result`
 	rows, err := dbpool.Query(ctx, query)
 	if err != nil {
 		return nil, err
--- views/013_check_summary.sql
+++ views/013_check_summary.sql
@@ -44,3 +44,50 @@ CREATE
 OR REPLACE TRIGGER checks_last_transition_time BEFORE
 UPDATE ON checks FOR EACH ROW WHEN (OLD.status IS DISTINCT FROM NEW.status)
 EXECUTE PROCEDURE update_last_transition_time_for_check ();
+
+-- check summary view
+CREATE OR REPLACE VIEW check_summary AS
+  SELECT
+    checks.id,
+    checks.canary_id,
+    json_build_object(
+      'passed', check_status_summary.passed,
+      'failed', check_status_summary.failed,
+      'last_pass', check_status_summary.last_pass,
+      'last_fail', check_status_summary.last_fail
+    ) AS uptime,
+    json_build_object('p99', check_status_summary.p99) AS latency,
+    checks.last_transition_time,
+    checks.type,
+    checks.icon,
+    checks.name,
+    checks.status,
+    checks.description,
+    canaries.namespace,
+    canaries.name as canary_name,
+    canaries.labels,
+    checks.severity,
+    checks.owner,
+    checks.last_runtime,
+    checks.created_at,
+    checks.updated_at,
+    checks.deleted_at,
+    checks.silenced_at
+  FROM
+    checks
+    INNER JOIN canaries ON checks.canary_id = canaries.id
+    INNER JOIN check_status_summary ON checks.id = check_status_summary.check_id;
+
+-- Check summary by component
+DROP FUNCTION IF EXISTS check_summary_for_component;
+
+CREATE OR REPLACE FUNCTION check_summary_for_component(id uuid) RETURNS setof check_summary
+AS $$
+  BEGIN
+    RETURN QUERY
+    SELECT check_summary.* FROM check_summary
+    INNER JOIN check_component_relationships
+      ON check_component_relationships.check_id = check_summary.id 
+    WHERE check_component_relationships.component_id = $1;
+  END;
+$$ language plpgsql;
