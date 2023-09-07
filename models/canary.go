bc0313e776df924aa519ccce368a7cf764af1dbc
Aditya Thebe <thebeaditya@gmail.com>
2023-09-07 19:27:10 +0545

    chore: add JSON tags to canary struct

--- models/canary.go
+++ models/canary.go
@@ -9,17 +9,17 @@ import (
 )
 
 type Canary struct {
-	ID        uuid.UUID `gorm:"default:generate_ulid()"`
-	AgentID   uuid.UUID `json:"agent_id,omitempty"`
-	Spec      types.JSON
-	Labels    types.JSONStringMap
-	Source    string
-	Name      string
-	Namespace string
-	Checks    types.JSONStringMap `gorm:"-"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time `json:"deleted_at,omitempty" time_format:"postgres_timestamp"`
+	ID        uuid.UUID           `json:"id" yaml:"id" gorm:"default:generate_ulid()"`
+	Name      string              `json:"name" yaml:"name"`
+	Namespace string              `json:"namespace" yaml:"namespace"`
+	AgentID   uuid.UUID           `json:"agent_id" yaml:"agent_id"`
+	Spec      types.JSON          `json:"spec" yaml:"spec"`
+	Labels    types.JSONStringMap `json:"labels,omitempty" yaml:"labels,omitempty"`
+	Source    string              `json:"source,omitempty" yaml:"source,omitempty"`
+	Checks    types.JSONStringMap `gorm:"-" json:"checks,omitempty" yaml:"checks,omitempty"`
+	CreatedAt time.Time           `json:"created_at" yaml:"created_at" time_format:"postgres_timestamp"`
+	UpdatedAt time.Time           `json:"updated_at" yaml:"updated_at" time_format:"postgres_timestamp"`
+	DeletedAt *time.Time          `json:"deleted_at,omitempty" yaml:"deleted_at,omitempty" time_format:"postgres_timestamp"`
 }
 
 func (c Canary) GetCheckID(checkName string) string {
