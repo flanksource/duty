02feb0d303e42fc986a004a484e56f9b278c78a5
Aditya Thebe <thebeaditya@gmail.com>
2023-09-08 14:06:43 +0545

    chore: addressed review comments

--- getters.go
+++ getters.go
@@ -2,21 +2,16 @@ package duty
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
-	"github.com/flanksource/commons/logger"
 	"github.com/flanksource/duty/models"
 	"github.com/patrickmn/go-cache"
 	"gorm.io/gorm"
 )
 
 // getterCache caches the results for all the getters in this file.
-var getterCache = cache.New(time.Second*30, time.Minute*5)
-
-type cachedItem struct {
-	resource any
-	notFound bool
-}
+var getterCache = cache.New(time.Second*90, time.Minute*5)
 
 func FindCachedAgent(ctx dbContext, agentID string) (*models.Agent, error) {
 	agent, err := findCachedEntity[models.Agent](ctx, agentID)
@@ -45,29 +40,51 @@ func FindCachedCanary(ctx dbContext, agentID string) (*models.Canary, error) {
 	return canary, nil
 }
 
+func FindCachedPerson(ctx dbContext, agentID string) (*models.Person, error) {
+	person, err := findCachedEntity[models.Person](ctx, agentID)
+	if err != nil {
+		return nil, err
+	}
+
+	return person, nil
+}
+
+func FindCacheComponent(ctx dbContext, componentID string) (*models.Component, error) {
+	component, err := findCachedEntity[models.Component](ctx, componentID)
+	if err != nil {
+		return nil, err
+	}
+
+	return component, nil
+}
+
+func FindCachedConfig(ctx dbContext, configID string) (*models.ConfigItem, error) {
+	config, err := findCachedEntity[models.ConfigItem](ctx, configID)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
 func findCachedEntity[T any](ctx dbContext, id string) (*T, error) {
 	if value, ok := getterCache.Get(id); ok {
-		if cache, ok := value.(cachedItem); ok {
-			if cache.notFound {
-				return nil, nil
-			}
-
-			return cache.resource.(*T), nil
+		if cache, ok := value.(*T); ok {
+			return cache, nil
 		} else {
-			logger.Warnf("Unexpected cached value type: %T", value)
+			return nil, fmt.Errorf("unexpected cached value type: %T", value)
 		}
 	}
 
 	var resource T
 	if err := ctx.DB().Where("id = ?", id).First(&resource).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			getterCache.SetDefault(id, cachedItem{notFound: true})
 			return nil, nil
 		}
 
 		return nil, err
 	}
 
-	getterCache.SetDefault(id, cachedItem{resource: &resource})
+	getterCache.SetDefault(id, &resource)
 	return &resource, nil
 }
