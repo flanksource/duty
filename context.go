8085772441858ce437e331d4c930907da239f086
Aditya Thebe <thebeaditya@gmail.com>
2023-09-20 12:03:03 +0545

    feat: add pg notify package

--- context.go
+++ context.go
@@ -3,10 +3,12 @@ package duty
 import (
 	"context"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"gorm.io/gorm"
 )
 
 type DBContext interface {
 	context.Context
 	DB() *gorm.DB
+	Pool() *pgxpool.Pool
 }
