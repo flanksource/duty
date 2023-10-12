540e2c74511b26b5480434573369b7b8edb86b26
Moshe Immerman <moshe@flanksource.com>
2023-10-12 14:39:11 +0300

    chore: add drop_push_queue_triggers

--- views/000_drop.sql
+++ views/000_drop.sql
@@ -1,3 +1,23 @@
-DROP VIEW IF EXISTS topology;
+DROP VIEW IF EXISTS topology; -- has dependencies on other views so we need to drop it first
 
--- has dependencies on other views so we need to drop it first
+DROP TRIGGER if exists check_statuses_change_to_event_queue on check_statuses;
+
+CREATE OR REPLACE FUNCTION drop_push_queue_triggers () returns void as $$
+DECLARE
+  triggerName TEXT;
+  tableName TEXT;
+BEGIN
+  FOR tableName, triggerName IN
+    SELECT event_object_table, trigger_name
+    FROM information_schema.triggers
+    WHERE trigger_name like '%s_change_to_event_queue'
+    GROUP BY event_object_table, trigger_name
+
+  LOOP
+    EXECUTE format('
+      DROP TRIGGER IF EXISTS %s ON %s',
+      triggerName, tableName
+    );
+  END LOOP;
+END;
+$$ LANGUAGE 'plpgsql' SECURITY DEFINER;
