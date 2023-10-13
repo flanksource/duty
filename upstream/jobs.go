b3fe69475006f6ebb9557483e5c86b20fef08a66
Aditya Thebe <thebeaditya@gmail.com>
2023-10-13 15:45:19 +0545

    feat: add check statuses syncing func (#300)
    * feat: add check statuses syncing func
    
    * feat: add new statuses for playbook
    
    * chore: remove playbook changes
    
    * chore: fetch check statuses in batches

---
+++ upstream/jobs.go
@@ -0,0 +1,45 @@
+package upstream
+
+import (
+	"fmt"
+
+	"github.com/flanksource/commons/logger"
+	"github.com/flanksource/duty"
+	"github.com/flanksource/duty/models"
+	"github.com/google/uuid"
+)
+
+// SyncCheckStatuses pushes check statuses, that haven't already been pushed, to upstream.
+func SyncCheckStatuses(ctx duty.DBContext, config UpstreamConfig, batchSize int) error {
+	client := NewUpstreamClient(config)
+
+	for {
+		var checkStatuses []models.CheckStatus
+		if err := ctx.DB().Select("check_statuses.*").
+			Joins("Left JOIN checks ON checks.id = check_statuses.check_id").
+			Where("checks.agent_id = ?", uuid.Nil).
+			Where("check_statuses.is_pushed IS FALSE").
+			Limit(batchSize).
+			Find(&checkStatuses).Error; err != nil {
+			return fmt.Errorf("failed to fetch checkstatuses: %w", err)
+		}
+
+		if len(checkStatuses) == 0 {
+			logger.Debugf("no check statuses to push")
+			return nil
+		}
+
+		logger.Tracef("pushing %d check statuses to upstream", len(checkStatuses))
+		if err := client.Push(ctx, &PushData{AgentName: config.AgentName, CheckStatuses: checkStatuses}); err != nil {
+			return fmt.Errorf("failed to push check statuses to upstream: %w", err)
+		}
+
+		for i := range checkStatuses {
+			checkStatuses[i].IsPushed = true
+		}
+
+		if err := ctx.DB().Save(&checkStatuses).Error; err != nil {
+			return fmt.Errorf("failed to save check statuses: %w", err)
+		}
+	}
+}
