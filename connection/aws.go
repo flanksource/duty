e4004d6788d935de3b8f42ef3984e83dce614f49
Aditya Thebe <thebeaditya@gmail.com>
2023-11-20 14:35:06 +0545

    feat: add new connection package
    * Moved few connection types from canary checker
    
    [skip ci]

---
+++ connection/aws.go
@@ -0,0 +1,85 @@
+package connection
+
+import (
+	"fmt"
+
+	"github.com/flanksource/duty/types"
+)
+
+type AWSConnection struct {
+	// ConnectionName of the connection. It'll be used to populate the endpoint, accessKey and secretKey.
+	ConnectionName string       `yaml:"connection,omitempty" json:"connection,omitempty"`
+	AccessKey      types.EnvVar `yaml:"accessKey" json:"accessKey,omitempty"`
+	SecretKey      types.EnvVar `yaml:"secretKey" json:"secretKey,omitempty"`
+	SessionToken   types.EnvVar `yaml:"sessionToken,omitempty" json:"sessionToken,omitempty"`
+	Region         string       `yaml:"region,omitempty" json:"region,omitempty"`
+	Endpoint       string       `yaml:"endpoint,omitempty" json:"endpoint,omitempty"`
+	// Skip TLS verify when connecting to aws
+	SkipTLSVerify bool `yaml:"skipTLSVerify,omitempty" json:"skipTLSVerify,omitempty"`
+	// glob path to restrict matches to a subset
+	ObjectPath string `yaml:"objectPath,omitempty" json:"objectPath,omitempty"`
+	// Use path style path: http://s3.amazonaws.com/BUCKET/KEY instead of http://BUCKET.s3.amazonaws.com/KEY
+	UsePathStyle bool `yaml:"usePathStyle,omitempty" json:"usePathStyle,omitempty"`
+}
+
+func (t *AWSConnection) GetUsername() types.EnvVar {
+	return t.AccessKey
+}
+
+func (t *AWSConnection) GetPassword() types.EnvVar {
+	return t.SecretKey
+}
+
+func (t *AWSConnection) GetProperties() map[string]string {
+	return map[string]string{
+		"region": t.Region,
+	}
+}
+
+func (t *AWSConnection) GetURL() types.EnvVar {
+	return types.EnvVar{ValueStatic: t.Endpoint}
+}
+
+// Populate populates an AWSConnection with credentials and other information.
+// If a connection name is specified, it'll be used to populate the endpoint, accessKey and secretKey.
+func (t *AWSConnection) Populate(ctx ConnectionContext) error {
+	if t.ConnectionName != "" {
+		connection, err := ctx.HydrateConnectionByURL(t.ConnectionName)
+		if err != nil {
+			return fmt.Errorf("could not parse EC2 access key: %v", err)
+		}
+
+		t.AccessKey.ValueStatic = connection.Username
+		t.SecretKey.ValueStatic = connection.Password
+		if t.Endpoint == "" {
+			t.Endpoint = connection.URL
+		}
+
+		t.SkipTLSVerify = connection.InsecureTLS
+		if t.Region == "" {
+			if region, ok := connection.Properties["region"]; ok {
+				t.Region = region
+			}
+		}
+	}
+
+	if accessKey, err := ctx.GetEnvValueFromCache(t.AccessKey); err != nil {
+		return fmt.Errorf("could not parse AWS access key id: %v", err)
+	} else {
+		t.AccessKey.ValueStatic = accessKey
+	}
+
+	if secretKey, err := ctx.GetEnvValueFromCache(t.SecretKey); err != nil {
+		return fmt.Errorf(fmt.Sprintf("Could not parse AWS secret access key: %v", err))
+	} else {
+		t.SecretKey.ValueStatic = secretKey
+	}
+
+	if sessionToken, err := ctx.GetEnvValueFromCache(t.SessionToken); err != nil {
+		return fmt.Errorf(fmt.Sprintf("Could not parse AWS session token: %v", err))
+	} else {
+		t.SessionToken.ValueStatic = sessionToken
+	}
+
+	return nil
+}
---
+++ connection/gcp.go
@@ -0,0 +1,35 @@
+package connection
+
+import (
+	"github.com/flanksource/duty/types"
+)
+
+type GCPConnection struct {
+	// ConnectionName of the connection. It'll be used to populate the endpoint and credentials.
+	ConnectionName string        `yaml:"connection,omitempty" json:"connection,omitempty"`
+	Endpoint       string        `yaml:"endpoint" json:"endpoint,omitempty"`
+	Credentials    *types.EnvVar `yaml:"credentials" json:"credentials,omitempty"`
+}
+
+func (g *GCPConnection) Validate() *GCPConnection {
+	if g == nil {
+		return &GCPConnection{}
+	}
+	return g
+}
+
+// HydrateConnection attempts to find the connection by name
+// and populate the endpoint and credentials.
+func (g *GCPConnection) HydrateConnection(ctx ConnectionContext) error {
+	connection, err := ctx.HydrateConnectionByURL(g.ConnectionName)
+	if err != nil {
+		return err
+	}
+
+	if connection != nil {
+		g.Credentials = &types.EnvVar{ValueStatic: connection.Certificate}
+		g.Endpoint = connection.URL
+	}
+
+	return nil
+}
