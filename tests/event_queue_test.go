8b0b5dfdc3afd830c6647eea4f4ca93739900cbe
Aditya Thebe <thebeaditya@gmail.com>
2024-01-03 18:57:42 +0545

    chore: make event queue related structs public

--- tests/event_queue_test.go
+++ tests/event_queue_test.go
@@ -1,44 +1,15 @@
 package tests
 
 import (
-	"time"
-
 	"github.com/flanksource/commons/logger"
+	"github.com/flanksource/duty/models"
 	ginkgo "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-type eventQueueSummary struct {
-	Name          string     `json:"name"`
-	Pending       int64      `json:"pending"`
-	Failed        int64      `json:"failed"`
-	AvgAttempts   int64      `json:"average_attempts"`
-	FirstFailure  *time.Time `json:"first_failure,omitempty"`
-	LastFailure   *time.Time `json:"last_failure,omitempty"`
-	MostCommonErr string     `json:"most_common_error,omitempty"`
-}
-
-func (t *eventQueueSummary) TableName() string {
-	return "event_queue_summary"
-}
-
-type pushQueueSummary struct {
-	Table         string     `json:"table"`
-	Pending       int64      `json:"pending"`
-	Failed        int64      `json:"failed"`
-	AvgAttempts   int64      `json:"average_attempts"`
-	FirstFailure  *time.Time `json:"first_failure,omitempty"`
-	LastFailure   *time.Time `json:"last_failure,omitempty"`
-	MostCommonErr string     `json:"most_common_error,omitempty"`
-}
-
-func (t *pushQueueSummary) TableName() string {
-	return "push_queue_summary"
-}
-
 var _ = ginkgo.Describe("Event queue views", func() {
 	ginkgo.It("should query event queue views", func() {
-		var summaries []eventQueueSummary
+		var summaries []models.EventQueueSummary
 		err := DefaultContext.DB().Find(&summaries).Error
 		Expect(err).ToNot(HaveOccurred())
 
@@ -46,7 +17,7 @@ var _ = ginkgo.Describe("Event queue views", func() {
 	})
 
 	ginkgo.It("should return deleted checks", func() {
-		var summaries []pushQueueSummary
+		var summaries []models.PushQueueSummary
 		err := DefaultContext.DB().Find(&summaries).Error
 		Expect(err).ToNot(HaveOccurred())
 
