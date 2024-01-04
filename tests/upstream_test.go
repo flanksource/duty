aceff580fda1d5774ec51ef4f332d0b44f10a7bf
Aditya Thebe <thebeaditya@gmail.com>
2024-01-04 09:44:18 +0545

    feat: add tests for changes & analyses syncer
    * migrated errors/http statuses from mission-control
    * migrated upstream handlers from mission-control

---
+++ tests/upstream_test.go
@@ -0,0 +1,137 @@
+package tests
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	ginkgo "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/patrickmn/go-cache"
+
+	"github.com/flanksource/duty/context"
+	"github.com/flanksource/duty/models"
+	"github.com/flanksource/duty/tests/setup"
+	"github.com/flanksource/duty/upstream"
+)
+
+var _ = ginkgo.Describe("Config Changes & Analyses sync test", ginkgo.Ordered, func() {
+	var upstreamCtx *context.Context
+	var echoCloser, drop func()
+	var upstreamConf upstream.UpstreamConfig
+	const agentName = "my-agent"
+
+	ginkgo.It("prepare upstream database", func() {
+		var err error
+		upstreamCtx, drop, err = setup.NewDB(DefaultContext, "upstream")
+		Expect(err).ToNot(HaveOccurred())
+
+		var changes int
+		err = upstreamCtx.DB().Select("COUNT(*)").Model(&models.ConfigChange{}).Scan(&changes).Error
+		Expect(err).ToNot(HaveOccurred())
+		Expect(changes).To(Equal(0))
+
+		var analyses int
+		err = upstreamCtx.DB().Select("COUNT(*)").Model(&models.ConfigAnalysis{}).Scan(&analyses).Error
+		Expect(err).ToNot(HaveOccurred())
+		Expect(analyses).To(Equal(0))
+
+		agent := models.Agent{Name: agentName}
+		err = upstreamCtx.DB().Create(&agent).Error
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	ginkgo.It("should setup upstream echo server", func() {
+		var port int
+		e := echo.New()
+		e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+			return func(c echo.Context) error {
+				c.SetRequest(c.Request().WithContext(upstreamCtx.Wrap(c.Request().Context())))
+				return next(c)
+			}
+		})
+
+		e.POST("/upstream/push", upstream.PushHandler(cache.New(time.Hour, time.Hour)))
+		e.GET("/upstream/pull/:agent_name", upstream.PullHandler([]string{"config_scrapers", "config_items"}))
+		e.GET("/upstream/status/:agent_name", upstream.StatusHandler([]string{"config_scrapers", "config_items"}))
+
+		port, echoCloser = setup.RunEcho(e)
+
+		upstreamConf = upstream.UpstreamConfig{
+			Host:      fmt.Sprintf("http://localhost:%d", port),
+			AgentName: agentName,
+		}
+	})
+
+	ginkgo.It("should push config items first to satisfy foregin keys for changes & analyses", func() {
+		reconciler := upstream.NewUpstreamReconciler(upstreamConf, 100)
+
+		count, err := reconciler.Sync(DefaultContext, "config_items")
+		Expect(err).To(BeNil())
+		Expect(count).To(Not(BeZero()))
+	})
+
+	ginkgo.It("should sync config_changes to upstream", func() {
+		{
+			var pushed int
+			err := DefaultContext.DB().Select("COUNT(*)").Where("is_pushed = true").Model(&models.ConfigChange{}).Scan(&pushed).Error
+			Expect(err).ToNot(HaveOccurred())
+			Expect(pushed).To(BeZero())
+		}
+
+		var changes int
+		err := upstreamCtx.DB().Select("COUNT(*)").Model(&models.ConfigChange{}).Scan(&changes).Error
+		Expect(err).ToNot(HaveOccurred())
+		Expect(changes).To(BeZero())
+
+		count, err := upstream.SyncConfigChanges(DefaultContext, upstreamConf, 10)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = upstreamCtx.DB().Select("COUNT(*)").Model(&models.ConfigChange{}).Scan(&changes).Error
+		Expect(err).ToNot(HaveOccurred())
+		Expect(changes).To(Equal(count))
+
+		{
+			var pending int
+			err := DefaultContext.DB().Select("COUNT(*)").Where("is_pushed = false").Model(&models.ConfigChange{}).Scan(&pending).Error
+			Expect(err).ToNot(HaveOccurred())
+			Expect(pending).To(BeZero())
+		}
+	})
+
+	ginkgo.It("should sync config_analyses to upstream", func() {
+		{
+			var pushed int
+			err := DefaultContext.DB().Select("COUNT(*)").Where("is_pushed = true").Model(&models.ConfigAnalysis{}).Scan(&pushed).Error
+			Expect(err).ToNot(HaveOccurred())
+			Expect(pushed).To(BeZero())
+		}
+
+		var analyses int
+		err := upstreamCtx.DB().Select("COUNT(*)").Model(&models.ConfigAnalysis{}).Scan(&analyses).Error
+		Expect(err).ToNot(HaveOccurred())
+		Expect(analyses).To(BeZero())
+
+		count, err := upstream.SyncConfigAnalyses(DefaultContext, upstreamConf, 10)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = upstreamCtx.DB().Select("COUNT(*)").Model(&models.ConfigAnalysis{}).Scan(&analyses).Error
+		Expect(err).ToNot(HaveOccurred())
+		Expect(analyses).To(Equal(count))
+
+		{
+			var pending int
+			err := DefaultContext.DB().Select("COUNT(*)").Where("is_pushed = false").Model(&models.ConfigAnalysis{}).Scan(&pending).Error
+			Expect(err).ToNot(HaveOccurred())
+			Expect(pending).To(BeZero())
+		}
+	})
+
+	ginkgo.It("should stop echo server ", func() {
+		echoCloser()
+	})
+
+	ginkgo.It("should drop upstream database ", func() {
+		drop()
+	})
+})
---
+++ upstream/commands.go
@@ -0,0 +1,124 @@
+package upstream
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/flanksource/commons/logger"
+	"github.com/flanksource/duty/context"
+	"github.com/flanksource/duty/models"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+func getAgent(ctx context.Context, name string) (*models.Agent, error) {
+	var t models.Agent
+	tx := ctx.DB().Where("name = ?", name).First(&t)
+	return &t, tx.Error
+}
+
+func createAgent(ctx context.Context, name string) (*models.Agent, error) {
+	a := models.Agent{Name: name}
+	tx := ctx.DB().Create(&a)
+	return &a, tx.Error
+}
+
+func GetOrCreateAgent(ctx context.Context, name string) (*models.Agent, error) {
+	a, err := getAgent(ctx, name)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			newAgent, err := createAgent(ctx, name)
+			if err != nil {
+				return nil, fmt.Errorf("failed to create agent: %w", err)
+			}
+			return newAgent, nil
+		}
+		return nil, err
+	}
+
+	return a, nil
+}
+
+func InsertUpstreamMsg(ctx context.Context, req *PushData) error {
+	batchSize := 100
+	db := ctx.DB()
+	if len(req.Topologies) > 0 {
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(req.Topologies, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting topologies: %w", err)
+		}
+	}
+
+	if len(req.Canaries) > 0 {
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(req.Canaries, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting canaries: %w", err)
+		}
+	}
+
+	// components are inserted one by one, instead of in a batch, because of the foreign key constraint with itself.
+	for _, c := range req.Components {
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(req.Components, batchSize).Error; err != nil {
+			logger.Errorf("error upserting component (id=%s): %v", c.ID, err)
+		}
+	}
+
+	if len(req.ComponentRelationships) > 0 {
+		cols := []clause.Column{{Name: "component_id"}, {Name: "relationship_id"}, {Name: "selector_id"}}
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true, Columns: cols}).CreateInBatches(req.ComponentRelationships, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting component_relationships: %w", err)
+		}
+	}
+
+	if len(req.ConfigScrapers) > 0 {
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(req.ConfigScrapers, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting config scrapers: %w", err)
+		}
+	}
+
+	// config items are inserted one by one, instead of in a batch, because of the foreign key constraint with itself.
+	for _, ci := range req.ConfigItems {
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(&ci, batchSize).Error; err != nil {
+			logger.Errorf("error upserting config item (id=%s): %v", ci.ID, err)
+		}
+	}
+
+	if len(req.ConfigRelationships) > 0 {
+		cols := []clause.Column{{Name: "related_id"}, {Name: "config_id"}, {Name: "selector_id"}}
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true, Columns: cols}).CreateInBatches(req.ConfigRelationships, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting config_relationships: %w", err)
+		}
+	}
+
+	if len(req.ConfigComponentRelationships) > 0 {
+		cols := []clause.Column{{Name: "component_id"}, {Name: "config_id"}}
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true, Columns: cols}).CreateInBatches(req.ConfigComponentRelationships, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting config_component_relationships: %w", err)
+		}
+	}
+
+	if len(req.ConfigChanges) > 0 {
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(req.ConfigChanges, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting config_changes: %w", err)
+		}
+	}
+
+	if len(req.ConfigAnalysis) > 0 {
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(req.ConfigAnalysis, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting config_analysis: %w", err)
+		}
+	}
+
+	if len(req.Checks) > 0 {
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true}).CreateInBatches(req.Checks, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting checks: %w", err)
+		}
+	}
+
+	if len(req.CheckStatuses) > 0 {
+		cols := []clause.Column{{Name: "check_id"}, {Name: "time"}}
+		if err := db.Clauses(clause.OnConflict{UpdateAll: true, Columns: cols}).CreateInBatches(req.CheckStatuses, batchSize).Error; err != nil {
+			return fmt.Errorf("error upserting check_statuses: %w", err)
+		}
+	}
+
+	return nil
+}
