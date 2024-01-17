0039d47d92abad50d0aa80e08266e327e1a38987
Aditya Thebe <thebeaditya@gmail.com>
2024-01-17 18:17:47 +0545

    feat: add new view to list all playbooks than an agent can run
    [skip ci]

--- views/018_playbooks.sql
+++ views/018_playbooks.sql
@@ -33,3 +33,25 @@ CREATE OR REPLACE TRIGGER playbook_updated_trigger
 AFTER UPDATE ON playbooks
 FOR EACH ROW
 EXECUTE PROCEDURE notify_playbook_update();
+
+-- List of all the playbooks that can be run by an agent
+CREATE OR REPLACE VIEW 
+playbooks_for_agent AS
+WITH interim AS (
+  SELECT
+    id,
+    jsonb_array_elements_text(spec -> 'runsOn') AS agent_name
+  FROM
+    playbooks
+  WHERE
+    spec ->> 'runsOn' IS NOT NULL
+)
+SELECT
+  interim.agent_name,
+  agents.person_id,
+  agents.id as agent_id,
+  json_agg(interim.id) AS playbook_ids
+FROM
+  interim
+  INNER JOIN agents ON interim.agent_name :: TEXT = agents.name
+GROUP BY agent_name, agents.person_id, agent_id
