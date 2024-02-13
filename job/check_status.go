972d3bb2554750dae38644f704e52c2592cd484a
Aditya Thebe <contact@adityathebe.com>
2024-02-13 10:45:43 +0545

    fix: check statuses cleanup job

--- job/check_status.go
+++ job/check_status.go
@@ -16,22 +16,22 @@ func RefreshCheckStatusSummaryAged(ctx context.Context) error {
 	return ctx.DB().Exec("REFRESH MATERIALIZED VIEW check_status_summary_aged").Error
 }
 
-func DeleteOldCheckStatuses(ctx context.Context, retention int) (error, int) {
+func DeleteOldCheckStatuses(ctx context.Context, retention int) (int, error) {
 	tx := ctx.DB().Exec(`DELETE FROM check_statuses WHERE (NOW() - time) > INTERVAL '1 day' * ?`, retention)
-	return tx.Error, int(tx.RowsAffected)
+	return int(tx.RowsAffected), tx.Error
 }
 
-func DeleteOldCheckStatuses1d(ctx context.Context, retention int) (error, int) {
-	tx := ctx.DB().Exec(`DELETE FROM check_statuses_1d WHERE (NOW() - time) > INTERVAL '1 day' * ?`, retention)
-	return tx.Error, int(tx.RowsAffected)
+func DeleteOldCheckStatuses1d(ctx context.Context, retention int) (int, error) {
+	tx := ctx.DB().Exec(`DELETE FROM check_statuses_1d WHERE (NOW() - created_at) > INTERVAL '1 day' * ?`, retention)
+	return int(tx.RowsAffected), tx.Error
 }
 
-func DeleteOldCheckStatuses1h(ctx context.Context, retention int) (error, int) {
-	tx := ctx.DB().Exec(`DELETE FROM check_statuses_1h WHERE (NOW() - time) > INTERVAL '1 day' * ?`, retention)
-	return tx.Error, int(tx.RowsAffected)
+func DeleteOldCheckStatuses1h(ctx context.Context, retention int) (int, error) {
+	tx := ctx.DB().Exec(`DELETE FROM check_statuses_1h WHERE (NOW() - created_at) > INTERVAL '1 day' * ?`, retention)
+	return int(tx.RowsAffected), tx.Error
 }
 
-func AggregateCheckStatus1d(ctx context.Context) (error, int) {
+func AggregateCheckStatus1d(ctx context.Context) (int, error) {
 	const query = `
 	SELECT
 		check_statuses.check_id,
@@ -50,9 +50,9 @@ func AggregateCheckStatus1d(ctx context.Context) (error, int) {
 	var err error
 	rows, err = ctx.DB().Raw(query, "day", 3*24).Rows() // Only look for aggregated data in the last 3 days
 	if err != nil {
-		return fmt.Errorf("error aggregating check statuses 1h: %w", err), 0
+		return 0, fmt.Errorf("error aggregating check statuses 1h: %w", err)
 	} else if rows.Err() != nil {
-		return fmt.Errorf("error aggregating check statuses 1h: %w", rows.Err()), 0
+		return 0, fmt.Errorf("error aggregating check statuses 1h: %w", rows.Err())
 	}
 	defer rows.Close()
 
@@ -61,18 +61,19 @@ func AggregateCheckStatus1d(ctx context.Context) (error, int) {
 		count++
 		var aggr models.CheckStatusAggregate1d
 		if err := rows.Scan(&aggr.CheckID, &aggr.CreatedAt, &aggr.Total, &aggr.Passed, &aggr.Failed, &aggr.Duration); err != nil {
-			return fmt.Errorf("error scanning aggregated check statuses: %w", err), 0
+			return 0, fmt.Errorf("error scanning aggregated check statuses: %w", err)
 		}
 
 		cols := []clause.Column{{Name: "check_id"}, {Name: "created_at"}}
 		if err := ctx.DB().Clauses(clause.OnConflict{Columns: cols, UpdateAll: true}).Create(aggr).Error; err != nil {
-			return fmt.Errorf("error upserting canaries: %w", err), 0
+			return 0, fmt.Errorf("error upserting canaries: %w", err)
 		}
 	}
-	return nil, count
+
+	return count, nil
 }
 
-func AggregateCheckStatus1h(ctx context.Context) (error, int) {
+func AggregateCheckStatus1h(ctx context.Context) (int, error) {
 	const query = `
 	SELECT
 		check_statuses.check_id,
@@ -91,9 +92,9 @@ func AggregateCheckStatus1h(ctx context.Context) (error, int) {
 	var err error
 	rows, err = ctx.DB().Raw(query, "hour", 3).Rows() // Only look for aggregated data in the last 3 hour
 	if err != nil {
-		return fmt.Errorf("error aggregating check statuses 1h: %w", err), 0
+		return 0, fmt.Errorf("error aggregating check statuses 1h: %w", err)
 	} else if rows.Err() != nil {
-		return fmt.Errorf("error aggregating check statuses 1h: %w", rows.Err()), 0
+		return 0, fmt.Errorf("error aggregating check statuses 1h: %w", rows.Err())
 	}
 	defer rows.Close()
 	count := 0
@@ -101,14 +102,14 @@ func AggregateCheckStatus1h(ctx context.Context) (error, int) {
 		count += 0
 		var aggr models.CheckStatusAggregate1h
 		if err := rows.Scan(&aggr.CheckID, &aggr.CreatedAt, &aggr.Total, &aggr.Passed, &aggr.Failed, &aggr.Duration); err != nil {
-			return fmt.Errorf("error scanning aggregated check statuses: %w", err), 0
+			return 0, fmt.Errorf("error scanning aggregated check statuses: %w", err)
 		}
 
 		cols := []clause.Column{{Name: "check_id"}, {Name: "created_at"}}
 		if err := ctx.DB().Clauses(clause.OnConflict{Columns: cols, UpdateAll: true}).Create(aggr).Error; err != nil {
-			return fmt.Errorf("error upserting canaries: %w", err), 0
+			return 0, fmt.Errorf("error upserting canaries: %w", err)
 		}
 	}
 
-	return nil, count
+	return count, nil
 }
