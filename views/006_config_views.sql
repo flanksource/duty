3f70c6b2b946a8db21f5e7de19974e66244f6787
Aditya Thebe <contact@adityathebe.com>
2024-02-20 18:34:09 +0545

    feat: add new func 'related_config_ids' and use that in 'related_configs'

--- views/006_config_views.sql
+++ views/006_config_views.sql
@@ -442,6 +442,43 @@ CREATE OR REPLACE VIEW config_changes_items AS
     LEFT JOIN config_items as ci ON cc.config_id = ci.id;
 
 
+-- related config ids
+DROP FUNCTION IF EXISTS related_config_ids(UUID, BOOLEAN);
+
+CREATE FUNCTION related_config_ids (
+  config_id UUID,
+  include_deleted_configs BOOLEAN
+)
+RETURNS TABLE (
+  relation TEXT,
+  relation_type TEXT,
+  id UUID
+) AS $$
+BEGIN
+  RETURN query
+    SELECT
+      config_relationships.relation,
+      'outgoing' AS relation_type,
+      c.id
+    FROM config_relationships
+      INNER JOIN configs AS c ON config_relationships.related_id = c.id AND ($2 OR c.deleted_at IS NULL)
+    WHERE
+      config_relationships.deleted_at IS NULL
+      AND config_relationships.config_id = $1
+    UNION
+    SELECT
+      config_relationships.relation,
+      'incoming' AS relation_type,
+      c.id
+    FROM config_relationships
+      INNER JOIN configs AS c ON config_relationships.config_id = c.id AND ($2 OR c.deleted_at IS NULL)
+    WHERE
+      config_relationships.deleted_at IS NULL
+      AND config_relationships.related_id = $1;
+END;
+$$ LANGUAGE plpgsql;
+
+-- related configs
 DROP FUNCTION IF EXISTS related_configs(UUID, BOOLEAN);
 
 CREATE FUNCTION related_configs (
@@ -456,8 +493,8 @@ RETURNS TABLE (
 BEGIN
   RETURN query
     SELECT
-      config_relationships.relation,
-      'outgoing' AS relation_type,
+      r.relation,
+      r.relation_type,
       jsonb_build_object(
         'id', c.id,
         'name', c.name, 
@@ -472,33 +509,7 @@ BEGIN
         'created_at', c.created_at, 
         'updated_at', c.updated_at
       ) AS config
-    FROM config_relationships
-      INNER JOIN configs AS c ON config_relationships.related_id = c.id AND ($2 OR c.deleted_at IS NULL)
-    WHERE
-      config_relationships.deleted_at IS NULL
-      AND config_relationships.config_id = $1
-    UNION
-    SELECT
-      config_relationships.relation,
-      'incoming' AS relation_type,
-       jsonb_build_object(
-        'id', c.id,
-        'name', c.name, 
-        'type', c.type, 
-        'tags', c.tags, 
-        'changes', c.changes,
-        'analysis', c.analysis,
-        'cost_per_minute', c.cost_per_minute,
-        'cost_total_1d', c.cost_total_1d,
-        'cost_total_7d', c.cost_total_7d,
-        'cost_total_30d', c.cost_total_30d,
-        'created_at', c.created_at, 
-        'updated_at', c.updated_at
-      ) AS config
-    FROM config_relationships
-      INNER JOIN configs AS c ON config_relationships.config_id = c.id AND ($2 OR c.deleted_at IS NULL)
-    WHERE
-      config_relationships.deleted_at IS NULL
-      AND config_relationships.related_id = $1;
+    FROM related_config_ids($1, $2) as r
+    LEFT JOIN configs AS c ON r.id = c.id;
 END;
 $$ LANGUAGE plpgsql;
