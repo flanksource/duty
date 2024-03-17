d04dd191a93579011485151f8e5632d7dda72564
Moshe Immermam <moshe@flanksource.com>
2024-03-17 12:27:16 +0200

    fix: don't push deletes of check_statuses

--- views/012_changelog_trigger_func.sql
+++ views/012_changelog_trigger_func.sql
@@ -14,8 +14,7 @@ DECLARE
         "config_items": 40,
         "config_component_relationships": 50,
         "component_relationships": 50,
-        "config_relationships": 50,
-        "check_statuses": 50
+        "config_relationships": 50
     }';
 BEGIN
   IF TG_OP != 'DELETE' THEN
@@ -31,7 +30,7 @@ BEGIN
     WHEN 'config_relationships' THEN
       payload = jsonb_build_object('related_id', rec.related_id, 'config_id', rec.config_id, 'selector_id', rec.selector_id);
     WHEN 'check_statuses' THEN
-      payload = jsonb_build_object('check_id', rec.check_id, 'time', rec.time);
+       RETURN NULL;
     ELSE
       payload = jsonb_build_object('id', rec.id);
   END CASE;
@@ -46,12 +45,12 @@ END;
 $$ LANGUAGE 'plpgsql' SECURITY DEFINER;
 
 -- Apply trigger
-DO $$ 
-DECLARE 
+DO $$
+DECLARE
   table_name TEXT;
-BEGIN 
-  FOR table_name IN 
-    SELECT t.table_name 
+BEGIN
+  FOR table_name IN
+    SELECT t.table_name
     FROM information_schema.tables  t
     WHERE t.table_schema = 'public' AND t.table_type = 'BASE TABLE'
       AND t.table_name IN (
@@ -63,10 +62,9 @@ BEGIN
         'config_items',
         'config_component_relationships',
         'component_relationships',
-        'config_relationships',
-        'check_statuses'
+        'config_relationships'
       )
-  LOOP 
+  LOOP
     EXECUTE format('
       CREATE OR REPLACE TRIGGER %1$I_change_to_event_queue
       AFTER DELETE ON %1$I
@@ -74,5 +72,5 @@ BEGIN
       EXECUTE PROCEDURE push_deletes_to_event_queue()',
       table_name
     );
-  END LOOP; 
+  END LOOP;
 END $$;
