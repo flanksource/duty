cf8d93b5843703c9ac70f539eee139a5e97e9828
Aditya Thebe <contact@adityathebe.com>
2024-03-21 10:44:40 +0545

    test: resource selector

--- types/resource_selector_test.go
+++ types/resource_selector_test.go
@@ -1,9 +1,13 @@
-package types
+package types_test
 
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/samber/lo"
+
+	"github.com/flanksource/duty/models"
+	"github.com/flanksource/duty/types"
 )
 
 func TestResourceSelector_Hash_Consistency(t *testing.T) {
@@ -11,12 +15,12 @@ func TestResourceSelector_Hash_Consistency(t *testing.T) {
 
 	tests := []struct {
 		name           string
-		resourceSelect ResourceSelector
+		resourceSelect types.ResourceSelector
 		expectedHash   string
 	}{
 		{
 			name: "Test Case 1",
-			resourceSelect: ResourceSelector{
+			resourceSelect: types.ResourceSelector{
 				ID:            "4775d837-727a-4386-9225-1fa2c167cc96",
 				Name:          "example",
 				Namespace:     "default",
@@ -40,6 +44,145 @@ func TestResourceSelector_Hash_Consistency(t *testing.T) {
 				if tt.expectedHash != actualHash {
 					t.Errorf("[%s] Hash mismatch. expected(%s) got(%s)", tt.name, tt.expectedHash, actualHash)
 				}
+			}
+		})
+	}
+}
+
+func TestResourceSelector_Matches(t *testing.T) {
+	tests := []struct {
+		name             string
+		resourceSelector types.ResourceSelector
+		selectable       types.ResourceSelectable
+		unselectable     types.ResourceSelectable
+	}{
+		{
+			name:             "Blank",
+			resourceSelector: types.ResourceSelector{},
+			selectable:       nil,
+			unselectable: models.ConfigItem{
+				Name:      lo.ToPtr("silverbullet"),
+				Namespace: lo.ToPtr("default"),
+			},
+		},
+		{
+			name: "ID",
+			resourceSelector: types.ResourceSelector{
+				ID: "4775d837-727a-4386-9225-1fa2c167cc96",
+			},
+			selectable: models.ConfigItem{
+				ID:   uuid.MustParse("4775d837-727a-4386-9225-1fa2c167cc96"),
+				Name: lo.ToPtr("silverbullet"),
+			},
+			unselectable: models.ConfigItem{
+				ID:   uuid.MustParse("5775d837-727a-4386-9225-1fa2c167cc96"),
+				Name: lo.ToPtr("silverbullet"),
+			},
+		},
+		{
+			name: "Namespace & Name",
+			resourceSelector: types.ResourceSelector{
+				Name:      "airsonic",
+				Namespace: "default",
+			},
+			selectable: models.ConfigItem{
+				Name:      lo.ToPtr("airsonic"),
+				Namespace: lo.ToPtr("default"),
+			},
+			unselectable: models.ConfigItem{
+				Name:      lo.ToPtr("silverbullet"),
+				Namespace: lo.ToPtr("default"),
+			},
+		},
+		{
+			name: "Types",
+			resourceSelector: types.ResourceSelector{
+				Types: []string{"Kubernetes::Pod"},
+			},
+			selectable: models.ConfigItem{
+				Name: lo.ToPtr("cert-manager"),
+				Type: lo.ToPtr("Kubernetes::Pod"),
+			},
+			unselectable: models.ConfigItem{
+				Name: lo.ToPtr("cert-manager"),
+				Type: lo.ToPtr("Kubernetes::Deployment"),
+			},
+		},
+		{
+			name: "Statuses",
+			resourceSelector: types.ResourceSelector{
+				Namespace: "default",
+				Statuses:  []string{"healthy"},
+			},
+			selectable: models.ConfigItem{
+				Namespace: lo.ToPtr("default"),
+				Status:    lo.ToPtr("healthy"),
+			},
+			unselectable: models.ConfigItem{
+				Namespace: lo.ToPtr("default"),
+				Status:    lo.ToPtr("unhealthy"),
+			},
+		},
+		{
+			name: "Labels",
+			resourceSelector: types.ResourceSelector{
+				Types: []string{"Kubernetes::Pod"},
+			},
+			selectable: models.ConfigItem{
+				Name: lo.ToPtr("cert-manager"),
+				Type: lo.ToPtr("Kubernetes::Pod"),
+			},
+			unselectable: models.ConfigItem{
+				Name: lo.ToPtr("cert-manager"),
+				Type: lo.ToPtr("Kubernetes::Deployment"),
+			},
+		},
+		{
+			name: "Label selector",
+			resourceSelector: types.ResourceSelector{
+				Namespace:     "default",
+				LabelSelector: "env=production",
+			},
+			selectable: models.ConfigItem{
+				Namespace:   lo.ToPtr("default"),
+				ConfigClass: "Cluster",
+				Tags: lo.ToPtr(types.JSONStringMap{
+					"env": "production",
+				}),
+			},
+			unselectable: models.ConfigItem{
+				Namespace:   lo.ToPtr("default"),
+				ConfigClass: "Cluster",
+				Tags: lo.ToPtr(types.JSONStringMap{
+					"env": "dev",
+				}),
+			},
+		},
+		{
+			name: "Field selector",
+			resourceSelector: types.ResourceSelector{
+				Namespace:     "default",
+				FieldSelector: "config_class=Cluster",
+			},
+			selectable: models.ConfigItem{
+				Namespace:   lo.ToPtr("default"),
+				ConfigClass: "Cluster",
+			},
+			unselectable: models.ConfigItem{
+				Namespace:   lo.ToPtr("default"),
+				ConfigClass: "VirtualMachine",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.selectable != nil && !tt.resourceSelector.Matches(tt.selectable) {
+				t.Errorf("expected to match")
+			}
+
+			if tt.unselectable != nil && tt.resourceSelector.Matches(tt.unselectable) {
+				t.Errorf("expected to not match")
 			}
 		})
 	}
