bee89617311e8b0a80e0e2ce2a04c1d706a7a8bc
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-04-24 16:19:14 +0530

    chore: agent resource cleanup

--- views/022_agents.sql
+++ views/022_agents.sql
@@ -54,6 +54,10 @@ RETURNS TRIGGER AS $$
 BEGIN
   IF TG_OP = 'DELETE' OR (TG_OP = 'UPDATE' AND OLD.deleted_at IS NULL AND NEW.deleted_at IS NOT NULL) THEN
     UPDATE access_tokens SET expires_at = NOW() WHERE person_id = (SELECT person_id FROM agents WHERE id = OLD.id);
+
+    IF OLD.cleanup = TRUE OR NEW.cleanup = TRUE THEN
+      PERFORM delete_agent_resources(OLD.id, NOW());
+    END IF;
   END IF;
 
   RETURN NULL;
@@ -64,3 +68,22 @@ CREATE OR REPLACE TRIGGER delete_agent_access_tokens
 AFTER UPDATE OR DELETE ON agents
 FOR EACH ROW
 EXECUTE PROCEDURE delete_access_tokens();
+
+-- Mark agent resources as deleted
+CREATE OR REPLACE FUNCTION delete_agent_resources(agentid UUID, delete_time TIMESTAMPTZ) 
+RETURNS VOID AS $$
+BEGIN
+    IF agentid IS NULL OR agentid = '00000000-0000-0000-0000-000000000000' THEN
+        RETURN;
+    END IF;
+
+    UPDATE topologies SET deleted_at = delete_time WHERE agent_id = agentid;
+    UPDATE components SET deleted_at = delete_time WHERE agent_id = agentid;
+    UPDATE canaries SET deleted_at = delete_time WHERE agent_id = agentid;
+    UPDATE checks SET deleted_at = delete_time WHERE agent_id = agentid;
+    UPDATE config_scrapers SET deleted_at = delete_time WHERE agent_id = agentid;
+    UPDATE config_items SET deleted_at = delete_time WHERE agent_id = agentid;
+    UPDATE logging_backends SET deleted_at = delete_time WHERE agent_id = agentid;
+
+END
+$$ LANGUAGE plpgsql;
