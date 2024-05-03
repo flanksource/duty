e1dc5b9da2bd02073db1a60a18b34a6c6bbc290b
Aditya Thebe <contact@adityathebe.com>
2024-05-03 18:24:13 +0545

    refactor: modify stale job history cleanup func
    This way we can call the function with a different stale age for
    running job histories.

--- job/cleanup.go
+++ job/cleanup.go
@@ -8,32 +8,15 @@ import (
 	"github.com/flanksource/duty/models"
 )
 
-func CleanupStaleHistoryJob(ctx context.Context, age time.Duration, name, resourceID string) *Job {
-	return &Job{
-		Context:    ctx,
-		Name:       "CleanupStaleJobHistory",
-		Schedule:   "@every 24h",
-		Singleton:  true,
-		JobHistory: true,
-		Retention:  RetentionFew,
-		RunNow:     true,
-		Fn: func(ctx JobRuntime) error {
-			count, err := cleanupStaleHistory(ctx.Context, age, name, resourceID)
-			if err != nil {
-				return err
-			}
-
-			ctx.History.SuccessCount = count
-			return nil
-		},
-	}
-}
-
-func cleanupStaleHistory(ctx context.Context, age time.Duration, name, resourceID string) (int, error) {
+func CleanupStaleHistory(ctx context.Context, age time.Duration, name, resourceID string, statuses ...string) (int, error) {
 	ctx = ctx.WithName(fmt.Sprintf("job=%s", name)).WithName(fmt.Sprintf("resourceID=%s", resourceID))
+
 	query := ctx.DB().Where("NOW() - time_start >= ?", age)
 	if name != "" {
 		query = query.Where("name = ?", name)
+	}
+	if len(statuses) != 0 {
+		query = query.Where("status IN ?", statuses)
 	}
 	if resourceID != "" {
 		query = query.Where("resource_id = ?", resourceID)
