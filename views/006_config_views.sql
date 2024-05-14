cb77a7e307bc9abba68be254bbaa66a81d5aadc3
Aditya Thebe <contact@adityathebe.com>
2024-05-14 07:42:57 +0545

    chore: split up config_relationship insertion into new file

--- views/006_config_views.sql
+++ views/006_config_views.sql
@@ -697,19 +697,3 @@ CREATE OR REPLACE TRIGGER fn_insert_parent_to_config_relationship
 AFTER INSERT OR UPDATE ON config_items
 FOR EACH ROW
 EXECUTE PROCEDURE insert_parent_to_config_relationship();
-
--- Add the hard relationship for records existing before the trigger was added
-INSERT INTO config_relationships (config_id, related_id, relation)
-SELECT parent.id "config_id", child.id "related_id", 'hard'
-FROM config_items child
-JOIN config_items parent 
-  ON child.parent_id = parent.id
-WHERE child.deleted_at IS NULL 
-  AND parent.deleted_at IS NULL
-  AND NOT EXISTS (
-    SELECT 1
-    FROM config_relationships cr
-    WHERE cr.config_id = parent.id
-      AND cr.related_id = child.id
-      AND cr.relation = 'hard'
-  );
