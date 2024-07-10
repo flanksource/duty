1ee4b7a5df5d77f539470698310d25ccbf7a2ff9
Aditya Thebe <contact@adityathebe.com>
2024-07-10 19:43:49 +0545

    test: make the generation deterministic

--- tests/config_generator.go
+++ tests/config_generator.go
@@ -13,46 +13,58 @@ import (
 	"gorm.io/gorm"
 )
 
-type ConfigGenerator struct {
-	PodPerDeployment, DeploymentPerNamespace, Namespaces, Nodes int
-	NumChangesPerConfig                                         int
-	NumInsightsPerConfig                                        int
-	HealthyPercentage                                           int
-	UnhealthyPercentage                                         int
-	UnknownPercentage                                           int
-	DeletedPercentage                                           int
-	Tags                                                        map[string]string
-	Generated                                                   struct {
-		Configs       []models.ConfigItem
-		Changes       []models.ConfigChange
-		Analysis      []models.ConfigAnalysis
-		Relationships []models.ConfigRelationship
+type ConfigTypeHealthRequirements struct {
+	HealthyPercentage   int
+	UnhealthyPercentage int
+	WarningPercentage   int
+	UnknownPercentage   int
+}
+
+func (t *ConfigTypeHealthRequirements) SetDefaults() {
+	if t.IsEmpty() {
+		t.HealthyPercentage = 100
 	}
+}
+
+func (t *ConfigTypeHealthRequirements) IsEmpty() bool {
+	return t.HealthyPercentage+t.UnhealthyPercentage+t.WarningPercentage+t.UnknownPercentage == 0
+}
+
+func (t *ConfigTypeHealthRequirements) IsValid() bool {
+	return t.HealthyPercentage+t.UnhealthyPercentage+t.WarningPercentage+t.UnknownPercentage == 100
+}
+
+type ConfigTypeRequirements struct {
+	Status ConfigTypeHealthRequirements
+
+	Count   int
+	Deleted int
+
+	NumChangesPerConfig  int
+	NumInsightsPerConfig int
+}
+
+type Generated struct {
+	Configs       []models.ConfigItem
+	Changes       []models.ConfigChange
+	Analysis      []models.ConfigAnalysis
+	Relationships []models.ConfigRelationship
+}
+
+func (t *Generated) Total() int {
+	return len(t.Configs) + len(t.Changes) + len(t.Analysis) + len(t.Relationships)
+}
+
+type ConfigGenerator struct {
+	Namespaces, Nodes                                                  ConfigTypeRequirements
+	PodsPerReplicaSet, ReplicaSetPerDeployment, DeploymentPerNamespace ConfigTypeRequirements
+	Tags                                                               map[string]string
+	Generated                                                          Generated
+
 	count int
 }
 
-func (generator *ConfigGenerator) GenerateStatus() *string {
-	randomNumber := rand.Intn(100)
-
-	if randomNumber <= generator.UnhealthyPercentage {
-		return lo.ToPtr("unhealthy")
-	} else if randomNumber <= generator.UnhealthyPercentage+generator.UnknownPercentage {
-		return lo.ToPtr("unknown")
-	} else {
-		return lo.ToPtr("healthy")
-	}
-}
-
-func (generator *ConfigGenerator) GenerateDeleted() *time.Time {
-	randomNumber := rand.Intn(100)
-
-	if randomNumber <= generator.DeletedPercentage {
-		return lo.ToPtr(time.Now().Add(-10 * 24 * time.Hour))
-	}
-	return nil
-}
-
-func (generator *ConfigGenerator) GenerateConfigItem(configType string, parent *models.ConfigItem) models.ConfigItem {
+func (generator *ConfigGenerator) GenerateConfigItem(configType, status string, deletedAt *time.Time, parent *models.ConfigItem, req ConfigTypeRequirements) models.ConfigItem {
 	changes := []models.ConfigChange{}
 	analysis := []models.ConfigAnalysis{}
 	generator.count++
@@ -60,17 +72,17 @@ func (generator *ConfigGenerator) GenerateConfigItem(configType string, parent *
 
 	item := models.ConfigItem{
 		ID:        uuid.New(),
-		DeletedAt: generator.GenerateDeleted(),
+		DeletedAt: deletedAt,
 		Type:      lo.ToPtr(configType),
 		Name:      lo.ToPtr(name),
-		Status:    generator.GenerateStatus(),
+		Status:    &status,
 		Tags:      generator.Tags,
 	}
 	if parent != nil {
 		item.ParentID = &parent.ID
 	}
 
-	for i := 1; i <= generator.NumChangesPerConfig; i++ {
+	for i := 1; i <= req.NumChangesPerConfig; i++ {
 		changes = append(changes, models.ConfigChange{
 			ID:         uuid.New().String(),
 			ConfigID:   item.ID.String(),
@@ -81,7 +93,7 @@ func (generator *ConfigGenerator) GenerateConfigItem(configType string, parent *
 		})
 	}
 
-	for i := 1; i <= generator.NumInsightsPerConfig; i++ {
+	for i := 1; i <= req.NumInsightsPerConfig; i++ {
 		analysis = append(analysis, models.ConfigAnalysis{
 			ID:           uuid.New(),
 			ConfigID:     item.ID,
@@ -108,27 +120,58 @@ func (generator *ConfigGenerator) Link(parent, child models.ConfigItem) {
 }
 
 func (generator *ConfigGenerator) GenerateKubernetes() {
+	cluster := generator.GenerateConfigItem("Kubernetes::Cluster", "unknown", nil, nil, ConfigTypeRequirements{})
+	nodes := generator.generateNodes(&cluster)
 
+	for i := 0; i < generator.Namespaces.Count; i++ {
+		ns := generator.GenerateConfigItem("Kubernetes::Namespace", "Healthy", deletedTime(i, generator.Namespaces.Deleted), &cluster, generator.Namespaces)
+		generator.generateDeployments(ns, nodes)
+	}
+}
+
+func (generator *ConfigGenerator) generateNodes(cluster *models.ConfigItem) []models.ConfigItem {
 	var nodes []models.ConfigItem
-
-	cluster := generator.GenerateConfigItem("Kubernetes::Cluster", nil)
-
-	for i := 0; i < generator.Nodes; i++ {
-		nodes = append(nodes, generator.GenerateConfigItem("Kubernetes::Node", &cluster))
+	nodeStatuses := genStatuses(generator.Nodes.Count, generator.Nodes.Status)
+	for i := 0; i < generator.Nodes.Count; i++ {
+		node := generator.GenerateConfigItem("Kubernetes::Node", nodeStatuses[i], deletedTime(i, generator.Nodes.Deleted), cluster, generator.Nodes)
+		nodes = append(nodes, node)
 	}
+	return nodes
+}
 
-	for i := 0; i < generator.Namespaces; i++ {
-		ns := generator.GenerateConfigItem("Kubernetes::Namespace", &cluster)
-
-		for j := 0; j < generator.DeploymentPerNamespace; j++ {
-			deploy := generator.GenerateConfigItem("Kubernetes::Deployment", &ns)
-
-			for k := 0; k < generator.PodPerDeployment; k++ {
-				pod := generator.GenerateConfigItem("Kubernetes::Pod", &deploy)
-				generator.Link(nodes[rand.Intn(len(nodes))], pod)
-			}
-		}
+func (generator *ConfigGenerator) generateDeployments(ns models.ConfigItem, nodes []models.ConfigItem) {
+	deploymentStatuses := genStatuses(generator.DeploymentPerNamespace.Count, generator.DeploymentPerNamespace.Status)
+	for j := 0; j < generator.DeploymentPerNamespace.Count; j++ {
+		deletedAt := getDeletedAt(ns.DeletedAt, j, generator.DeploymentPerNamespace.Deleted)
+		deploy := generator.GenerateConfigItem("Kubernetes::Deployment", deploymentStatuses[j], deletedAt, &ns, generator.DeploymentPerNamespace)
+		generator.generateReplicaSets(deploy, nodes)
 	}
+}
+
+func (generator *ConfigGenerator) generateReplicaSets(deploy models.ConfigItem, nodes []models.ConfigItem) {
+	replicaSetStatuses := genStatuses(generator.ReplicaSetPerDeployment.Count, generator.ReplicaSetPerDeployment.Status)
+	for k := 0; k < generator.ReplicaSetPerDeployment.Count; k++ {
+		deletedAt := getDeletedAt(deploy.DeletedAt, k, generator.ReplicaSetPerDeployment.Deleted)
+		replicaSet := generator.GenerateConfigItem("Kubernetes::ReplicaSet", replicaSetStatuses[k], deletedAt, &deploy, generator.ReplicaSetPerDeployment)
+		generator.generatePods(replicaSet, deploy, nodes)
+	}
+}
+
+func (generator *ConfigGenerator) generatePods(replicaSet, deploy models.ConfigItem, nodes []models.ConfigItem) {
+	podStatuses := genStatuses(generator.PodsPerReplicaSet.Count, generator.PodsPerReplicaSet.Status)
+	for l := 0; l < generator.PodsPerReplicaSet.Count; l++ {
+		deletedAt := getDeletedAt(replicaSet.DeletedAt, l, generator.PodsPerReplicaSet.Deleted)
+		pod := generator.GenerateConfigItem("Kubernetes::Pod", podStatuses[l], deletedAt, &replicaSet, generator.PodsPerReplicaSet)
+		generator.Link(deploy, pod)
+		generator.Link(nodes[rand.Intn(len(nodes))], pod)
+	}
+}
+
+func getDeletedAt(parentDeletedAt *time.Time, currentIndex, deletedNeeded int) *time.Time {
+	if parentDeletedAt != nil {
+		return parentDeletedAt
+	}
+	return deletedTime(currentIndex, deletedNeeded)
 }
 
 func (generator *ConfigGenerator) Save(db *gorm.DB) error {
@@ -140,5 +183,48 @@ func (generator *ConfigGenerator) Save(db *gorm.DB) error {
 	tx.CreateInBatches(generator.Generated.Analysis, 100)
 
 	return tx.Commit().Error
+}
 
+func (generator *ConfigGenerator) Destroy(db *gorm.DB) error {
+	tx := db.Begin()
+
+	tx.Delete(&generator.Generated.Analysis)
+	tx.Delete(&generator.Generated.Changes)
+	tx.Delete(&generator.Generated.Relationships)
+	tx.Delete(&generator.Generated.Configs)
+	return tx.Commit().Error
+}
+
+func deletedTime(currentIndex int, deletedNeeded int) *time.Time {
+	if currentIndex < deletedNeeded {
+		return lo.ToPtr(time.Now().Add(-time.Duration(rand.Intn(60*24)) * time.Minute))
+	}
+
+	return nil
+}
+
+// genStatuses generates a slice of status strings based on the provided ConfigTypeHealthRequirements.
+func genStatuses(total int, req ConfigTypeHealthRequirements) []string {
+	req.SetDefaults()
+
+	output := make([]string, 0, total)
+	healthyCount := int(float64(total) * float64(req.HealthyPercentage/100))
+	unhealthyCount := int(float64(total) * float64(req.UnhealthyPercentage/100))
+	warningCount := int(float64(total) * float64(req.WarningPercentage/100))
+	unknownCount := int(float64(total) * float64(req.WarningPercentage/100))
+
+	for i := 0; i < healthyCount; i++ {
+		output = append(output, "healthy")
+	}
+	for i := 0; i < unhealthyCount; i++ {
+		output = append(output, "unhealthy")
+	}
+	for i := 0; i < warningCount; i++ {
+		output = append(output, "warning")
+	}
+	for i := 0; i < unknownCount; i++ {
+		output = append(output, "unknown")
+	}
+
+	return output
 }
