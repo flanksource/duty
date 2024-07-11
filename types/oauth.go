21c069acb8588940f7c88d29fcb248c586b56e8a
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-07-11 18:07:39 +0530

    chore: sanitize oauth

--- types/oauth.go
+++ types/oauth.go
@@ -1,8 +1,7 @@
 package types
 
 import (
-	"encoding/json"
-	"fmt"
+	"strings"
 
 	"github.com/flanksource/commons/collections"
 )
@@ -20,28 +19,10 @@ func (o OAuth) IsEmpty() bool {
 	return o.ClientID.IsEmpty() || o.ClientSecret.IsEmpty() || o.TokenURL == ""
 }
 
-// PopulateFromProperties needs properties to be hydrated
-func (o *OAuth) PopulateFromProperties(props map[string]string) error {
-	o.ClientID.ValueStatic = props["clientID"]
-	o.ClientSecret.ValueStatic = props["clientSecret"]
-	o.TokenURL = props["tokenURL"]
-	if props["scope"] != "" {
-		if err := json.Unmarshal([]byte(props["scopes"]), &o.Scopes); err != nil {
-			return fmt.Errorf("error unmarshaling scopes:%s in oauth: %w", props["scopes"], err)
-		}
-	}
-	if props["params"] != "" {
-		if err := json.Unmarshal([]byte(props["params"]), &o.Params); err != nil {
-			return fmt.Errorf("error unmarshaling params:%s in oauth: %w", props["params"], err)
-		}
-	}
-	return nil
-}
-
 func (o OAuth) AsProperties() JSONStringMap {
 	var scopes, params string
 	if o.Scopes != nil {
-		scopes, _ = collections.StructToJSON(o.Scopes)
+		scopes = strings.Join(o.Scopes, ",")
 	}
 	if o.Params != nil {
 		params, _ = collections.StructToJSON(o.Params)
