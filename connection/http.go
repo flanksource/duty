53f4a9600f25b2c47f427f6d2cf9ad27bbae3cf4
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-07-11 09:32:47 +0530

    chore: tls config for http connection

--- connection/http.go
+++ connection/http.go
@@ -3,6 +3,7 @@ package connection
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/flanksource/commons/collections"
 	"github.com/flanksource/commons/http"
@@ -12,12 +13,30 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type TLSConfig struct {
+	// InsecureSkipVerify controls whether a client verifies the server's
+	// certificate chain and host name
+	InsecureSkipVerify bool `json:"insecureSkipVerify,omitempty" yaml:"insecureSkipVerify,omitempty"`
+	// HandshakeTimeout defaults to 10 seconds
+	HandshakeTimeout time.Duration `json:"handshakeTimeout,omitempty" yaml:"handshakeTimeout,omitempty"`
+	// PEM encoded certificate of the CA to verify the server certificate
+	CA types.EnvVar `json:"ca,omitempty" yaml:"ca,omitempty"`
+	// PEM encoded client certificate
+	Cert types.EnvVar `json:"cert,omitempty" yaml:"cert,omitempty"`
+	// PEM encoded client private key
+	Key types.EnvVar `json:"key,omitempty" yaml:"key,omitempty"`
+}
+
+func (t TLSConfig) IsEmpty() bool {
+	return t.CA.IsEmpty() || t.Cert.IsEmpty() || t.Key.IsEmpty()
+}
+
 type HTTPConnection struct {
 	types.Authentication `json:",inline"`
 	URL                  string       `json:"url,omitempty" yaml:"url,omitempty"`
 	Bearer               types.EnvVar `json:"bearer,omitempty" yaml:"bearer,omitempty"`
 	OAuth                types.OAuth  `json:"oauth,omitempty" yaml:"oauth,omitempty"`
-	//TLS                  TLSConfig    `yaml:"tls,omitempty" json:"tls,omitempty"`
+	TLS                  TLSConfig    `json:"tls,omitempty" yaml:"tls,omitempty"`
 }
 
 func (h *HTTPConnection) Hydrate(ctx ConnectionContext, namespace string) (*HTTPConnection, error) {
@@ -44,11 +63,24 @@ func (h *HTTPConnection) Hydrate(ctx ConnectionContext, namespace string) (*HTTP
 	if err != nil {
 		return h, err
 	}
+
+	h.TLS.Key.ValueStatic, err = ctx.GetEnvValueFromCache(h.TLS.Key, namespace)
+	if err != nil {
+		return h, err
+	}
+	h.TLS.CA.ValueStatic, err = ctx.GetEnvValueFromCache(h.TLS.CA, namespace)
+	if err != nil {
+		return h, err
+	}
+	h.TLS.Cert.ValueStatic, err = ctx.GetEnvValueFromCache(h.TLS.Cert, namespace)
+	if err != nil {
+		return h, err
+	}
 	return h, nil
 }
 
 // CreateHTTPClient requires a hydrated connection
-func CreateHTTPClient(ctx ConnectionContext, conn HTTPConnection) *http.Client {
+func CreateHTTPClient(ctx ConnectionContext, conn HTTPConnection) (*http.Client, error) {
 	client := http.NewClient()
 	if !conn.Authentication.IsEmpty() {
 		client.Auth(conn.GetUsername(), conn.GetPassword())
@@ -64,7 +96,20 @@ func CreateHTTPClient(ctx ConnectionContext, conn HTTPConnection) *http.Client {
 		})
 	}
 
-	return client
+	if !conn.TLS.IsEmpty() {
+		_, err := client.TLSConfig(http.TLSConfig{
+			CA:                 conn.TLS.CA.ValueStatic,
+			Cert:               conn.TLS.Cert.ValueStatic,
+			Key:                conn.TLS.Key.ValueStatic,
+			InsecureSkipVerify: conn.TLS.InsecureSkipVerify,
+			HandshakeTimeout:   conn.TLS.HandshakeTimeout,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("error setting tls config: %w")
+		}
+	}
+
+	return client, nil
 }
 
 func NewHTTPConnection(ctx ConnectionContext, conn models.Connection) (HTTPConnection, error) {
