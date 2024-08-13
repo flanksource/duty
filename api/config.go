072ef2a93dddca8106c2f5409c340da8d0b43801
Moshe Immermam <moshe@flanksource.com>
2024-08-13 11:19:33 +0300

    chore: add k8s, otel, postgrest helpers

---
+++ api/config.go
@@ -0,0 +1,34 @@
+package api
+
+var DefaultConfig = Config{
+	Postgrest: PostgrestConfig{
+		Version:    "v10.0.0",
+		DBAnonRole: "postgrest_api",
+		Port:       3000,
+		MaxRows:    2000,
+	},
+}
+
+type Config struct {
+	ConnectionString, Schema string
+	SkipMigrations           bool
+	SkipMigrationFiles       []string
+	DisableKubernetes        bool
+	Namespace                string
+	Postgrest                PostgrestConfig
+	LogLevel                 string
+}
+
+type PostgrestConfig struct {
+	Port       int
+	Disable    bool
+	LogLevel   string
+	URL        string
+	Version    string
+	JWTSecret  string
+	DBAnonRole string
+
+	// A hard limit to the number of rows PostgREST will fetch from a view, table, or stored procedure.
+	// Limits payload size for accidental or malicious requests.
+	MaxRows int
+}
---
+++ kubernetes/k8s.go
@@ -0,0 +1,63 @@
+package kubernetes
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/flanksource/commons/files"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+var Nil = fake.NewSimpleClientset()
+
+func NewClient() (kubernetes.Interface, error) {
+	kubeconfig := os.Getenv("KUBECONFIG")
+	if kubeconfig == "" {
+		kubeconfig = os.ExpandEnv("$HOME/.kube/config")
+	}
+
+	if !files.Exists(kubeconfig) {
+		if config, err := rest.InClusterConfig(); err == nil {
+			return kubernetes.NewForConfig(config)
+		} else {
+			return nil, fmt.Errorf("cannot find kubeconfig")
+		}
+	}
+
+	data, err := os.ReadFile(kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	restConfig, err := clientcmd.RESTConfigFromKubeConfig(data)
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(restConfig)
+}
+
+func NewClientWithConfig(kubeConfig string) (kubernetes.Interface, error) {
+	getter := func() (*clientcmdapi.Config, error) {
+		clientCfg, err := clientcmd.NewClientConfigFromBytes([]byte(kubeConfig))
+		if err != nil {
+			return nil, err
+		}
+
+		apiCfg, err := clientCfg.RawConfig()
+		if err != nil {
+			return nil, err
+		}
+
+		return &apiCfg, nil
+	}
+
+	config, err := clientcmd.BuildConfigFromKubeconfigGetter("", getter)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate rest config: %w", err)
+	}
+
+	return kubernetes.NewForConfig(config)
+}
