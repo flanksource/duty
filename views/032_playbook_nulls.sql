c47892b299cf95a445df481877eafba173c3a653
Moshe Immermam <moshe@flanksource.com>
2024-08-16 08:53:58 +0300

    chore: fix playbook dup migration

--- views/032_playbook_nulls.sql
+++ views/032_playbook_nulls.sql
@@ -1,1 +1,16 @@
 UPDATE playbooks set category = '' WHERE category IS NULL;
+WITH duplicates AS (
+    SELECT
+        name,
+        namespace,
+        category,
+        ROW_NUMBER() OVER (PARTITION BY name, namespace, category ORDER BY id) as row_num
+    FROM playbooks
+)
+UPDATE playbooks
+SET name = playbooks.name || '_' || right(id::text, 4)
+FROM duplicates
+WHERE playbooks.name = duplicates.name
+  AND playbooks.namespace = duplicates.namespace
+  AND playbooks.category = duplicates.category
+  AND duplicates.row_num > 1;
