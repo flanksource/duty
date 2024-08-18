f3e629ecc6719d0eef2194e56bbfc85610e4ccad
Moshe Immermam <moshe@flanksource.com>
2024-08-18 22:30:01 +0300

    feat: add /debug/cron, logger and property endpoints

--- echo/debug.go
+++ echo/debug.go
@@ -1,16 +1,28 @@
 package echo
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 	"net/http/pprof"
+	"strings"
+	"time"
 
 	nethttp "net/http"
 
 	"github.com/flanksource/commons/logger"
+	"github.com/flanksource/commons/properties"
+	"github.com/flanksource/duty/context"
 	"github.com/google/gops/agent"
 	"github.com/labstack/echo/v4"
+	"github.com/robfig/cron/v3"
 )
+
+var Crons []*cron.Cron
+
+func RegisterCron(cron *cron.Cron) {
+	Crons = append(Crons, cron)
+}
 
 func init() {
 	// disables default handlers registered by importing net/http/pprof.
@@ -37,7 +49,8 @@ func RestrictToLocalhost(next echo.HandlerFunc) echo.HandlerFunc {
 	}
 }
 
-func AddDebugHandlers(e *echo.Echo) {
+func AddDebugHandlers(e *echo.Echo, rbac echo.MiddlewareFunc) {
+
 	// Add pprof routes with localhost restriction
 	pprofGroup := e.Group("/debug/pprof")
 	pprofGroup.Use(RestrictToLocalhost)
@@ -46,4 +59,126 @@ func AddDebugHandlers(e *echo.Echo) {
 	pprofGroup.GET("/profile*", echo.WrapHandler(http.HandlerFunc(pprof.Profile)))
 	pprofGroup.GET("/symbol*", echo.WrapHandler(http.HandlerFunc(pprof.Symbol)))
 	pprofGroup.GET("/trace*", echo.WrapHandler(http.HandlerFunc(pprof.Trace)))
+
+	debug := e.Group("/debug/", rbac)
+
+	debug.GET("/debug/loggers", func(c echo.Context) error {
+		c.JSON(200, logger.GetNamedLoggingLevels())
+		return nil
+	})
+
+	debug.POST("/debug/loggers", func(c echo.Context) error {
+		logName := c.Request().FormValue("logger")
+		logLevel := c.Request().FormValue("level")
+		duration := c.Request().FormValue("duration")
+		currentLevel := logger.GetLogger(logName).GetLevel()
+		if duration != "" {
+			durationInt, err := time.ParseDuration(duration)
+			if err != nil {
+				return c.JSON(http.StatusBadRequest, err)
+			}
+			logger.Infof("Setting logger %s level to %s for %v", logName, logLevel, duration)
+
+			go func() {
+				time.Sleep(durationInt)
+				logger.GetLogger(logName).SetLogLevel(currentLevel)
+			}()
+		}
+		if logName != "" && logLevel != "" {
+			if duration == "" {
+				logger.Infof("Setting logger %s level to %s", logName, logLevel)
+
+			}
+			logger.GetLogger(logName).SetLogLevel(logLevel)
+			return c.String(http.StatusOK, fmt.Sprintf("Changed %s from %s to %s", logName, currentLevel, logLevel))
+		} else {
+			return c.String(http.StatusBadRequest, "logger name or level is missing")
+		}
+	})
+
+	debug.GET("/debug/properties", func(c echo.Context) error {
+		ctx := c.Request().Context().(context.Context)
+		props := ctx.Properties().SupportedProperties()
+		return c.JSON(200, props)
+	})
+
+	debug.POST("/debug/property", func(c echo.Context) error {
+		id := c.Request().FormValue("name")
+		value := c.Request().FormValue("value")
+		if id != "" && value != "" {
+			properties.Set(id, value)
+			return c.NoContent(http.StatusOK)
+		} else {
+			return c.JSON(http.StatusBadRequest, fmt.Errorf("property id / value is missing"))
+		}
+	})
+
+	debug.POST("/debug/cron/run", func(c echo.Context) error {
+		name := c.Request().FormValue("name")
+		names := []string{}
+		for _, cron := range Crons {
+			for _, e := range cron.Entries() {
+				entry := toEntry(&e)
+				names = append(names, entry.GetName())
+				if entry.GetName() == name {
+					logger.Infof("Running %s now", name)
+					e.Job.Run()
+					return c.NoContent(http.StatusCreated)
+				}
+			}
+		}
+		return c.String(http.StatusNotFound, fmt.Sprintf("Cron job with name %s not found in %s", name, strings.Join(names, ", ")))
+
+	})
+
+	debug.GET("/debug/cron", CronDetailsHandler())
+}
+
+type JobCronEntry struct {
+	Context   map[string]any `json:"context"`
+	ID        int            `json:"id"`
+	LastRan   time.Time      `json:"last_ran,omitempty"`
+	Schedule  string         `json:"schedule"`
+	NextRun   time.Time      `json:"next_run"`
+	NextRunIn string         `json:"next_run_in"`
+}
+
+func (j JobCronEntry) GetName() string {
+	if name, ok := j.Context["name"]; ok {
+		return name.(string)
+	}
+
+	return ""
+}
+
+func toEntry(e *cron.Entry) JobCronEntry {
+	entry := JobCronEntry{
+		Schedule:  fmt.Sprintf("%v", e.Schedule),
+		LastRan:   e.Prev,
+		NextRun:   e.Next,
+		NextRunIn: time.Until(e.Next).String(),
+	}
+
+	switch v := e.Job.(type) {
+	case context.ContextAccessor:
+		entry.Context = v.Context()
+	case context.ContextAccessor2:
+		entry.Context = v.GetContext()
+	default:
+		entry.Context = map[string]any{"name": fmt.Sprintf("%v", e.Job)}
+	}
+	return entry
+}
+
+func CronDetailsHandler() func(c echo.Context) error {
+	return func(c echo.Context) error {
+		var entries []JobCronEntry
+		for _, cron := range Crons {
+			for _, e := range cron.Entries() {
+				entries = append(entries, toEntry(&e))
+			}
+		}
+
+		return c.JSON(http.StatusOK, entries)
+	}
 }
