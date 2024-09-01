3d5ee3baffcb09c341822791c668c0b7fe9e3d63
Moshe Immermam <moshe@flanksource.com>
2024-09-02 00:30:55 +0300

    feat: add common shell package

---
+++ shell/interpreter.go
@@ -0,0 +1,84 @@
+package shell
+
+import (
+	"bufio"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/flanksource/duty/context"
+)
+
+var DefaultInterpreter string
+var DefaultInterpreterArgs []string
+
+func init() {
+	DefaultInterpreter, DefaultInterpreterArgs = DetectDefaultInterpreter()
+}
+
+// CreateCommandFromScript creates an os/exec.Cmd from the script, using the interpreter specified in the shebang line if present.
+func CreateCommandFromScript(ctx context.Context, script string) (*exec.Cmd, error) {
+	interpreter, args := DetectInterpreterFromShebang(script)
+	script = TrimLine(script, "#!")
+	if script == "" {
+		return nil, ctx.Oops().Errorf("empty script")
+	}
+	args = append(args, script)
+	return exec.CommandContext(ctx, interpreter, args...), nil
+}
+
+func TrimLine(lines string, prefix string) string {
+	s := []string{}
+	for _, line := range strings.Split(lines, "\n") {
+		if !strings.HasPrefix(line, prefix) {
+			s = append(s, line)
+		}
+	}
+	return strings.Join(s, "\n")
+}
+
+// DetectInterpreterFromShebang reads the first line of the script to detect the interpreter from the shebang line.
+func DetectInterpreterFromShebang(script string) (string, []string) {
+	reader := strings.NewReader(script)
+	scanner := bufio.NewScanner(reader)
+	if scanner.Scan() {
+		firstLine := scanner.Text()
+		if strings.HasPrefix(firstLine, "#!") {
+			parts := strings.Fields(strings.TrimSpace(firstLine[2:]))
+			if len(parts) == 0 {
+				return "", nil
+			} else if len(parts) == 1 {
+				// No args, just interpreter and assume it supports the -c flag
+				return parts[0], []string{"-c"}
+			}
+			return parts[0], parts[1:]
+		}
+	}
+	return DefaultInterpreter, DefaultInterpreterArgs
+}
+
+// DetectDefaultInterpreter detects the default interpreter based on the OS.
+func DetectDefaultInterpreter() (string, []string) {
+	switch runtime.GOOS {
+	case "windows":
+		// Check for PowerShell on Windows
+		if _, err := exec.LookPath("pwsh.exe"); err == nil {
+			return "pwsh.exe", []string{"-c"}
+		}
+		// Fallback to cmd if PowerShell is not found
+		if _, err := exec.LookPath("cmd.exe"); err == nil {
+			return "cmd.exe", []string{"-c"}
+		}
+
+	default:
+		// Check for Bash on Unix-like systems
+		if _, err := exec.LookPath("bash"); err == nil {
+			return "bash", []string{"-c"}
+		}
+		// Fallback to sh if Bash is not found
+		if _, err := exec.LookPath("sh"); err == nil {
+			return "sh", []string{"-c"}
+		}
+	}
+	return "", nil
+}
---
+++ shell/shell.go
@@ -0,0 +1,224 @@
+package shell
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	osExec "os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/flanksource/artifacts"
+	fileUtils "github.com/flanksource/commons/files"
+	"github.com/flanksource/commons/hash"
+	"github.com/flanksource/commons/logger"
+	"github.com/flanksource/commons/utils"
+	"github.com/flanksource/duty/connection"
+	"github.com/flanksource/duty/context"
+	"github.com/flanksource/duty/types"
+	"github.com/samber/lo"
+	"github.com/samber/oops"
+)
+
+var checkoutLocks = utils.NamedLock{}
+
+type Exec struct {
+	Script      string
+	Connections connection.ExecConnections
+	Checkout    *connection.GitConnection
+	Artifacts   []Artifact
+	EnvVars     []types.EnvVar
+}
+
+// +kubebuilder:object:generate=true
+type Artifact struct {
+	Path string `json:"path" yaml:"path" template:"true"`
+}
+
+type ExecDetails struct {
+	Error    error    `json:"-"`
+	Stdout   string   `json:"stdout"`
+	Stderr   string   `json:"stderr"`
+	ExitCode int      `json:"exitCode"`
+	Path     string   `json:"path"`
+	Args     []string `json:"args"`
+	// Any extra details about the command execution, e.g. git commit id, etc.
+	Extra     map[string]any       `json:"extra,omitempty"`
+	Artifacts []artifacts.Artifact `json:"-" yaml:"-"`
+}
+
+func (e ExecDetails) String() string {
+	return fmt.Sprintf("%s %s exit=%d stdout=%s stderr=%s", e.Path, e.Args, e.ExitCode, e.Stdout, e.Stderr)
+}
+
+func (e *ExecDetails) GetArtifacts() []artifacts.Artifact {
+	if e == nil {
+		return nil
+	}
+	return e.Artifacts
+}
+
+func Run(ctx context.Context, exec Exec) (*ExecDetails, error) {
+	cmd, err := CreateCommandFromScript(ctx, exec.Script)
+	if err != nil {
+		return nil, oops.Hint(exec.Script).Wrap(err)
+	}
+
+	ctx.Logger.V(3).Infof("running: %s %s", cmd.Path, lo.Map(cmd.Args, func(arg string, _ int) string { return strings.TrimSpace(arg) }))
+	envParams, err := prepareEnvironment(ctx, exec)
+	if err != nil {
+		return nil, ctx.Oops().Wrap(err)
+	}
+
+	if len(envParams.envs) != 0 {
+		ctx.Logger.V(6).Infof("using environment %s", logger.Pretty(envParams.envs))
+		cmd.Env = append(os.Environ(), envParams.envs...)
+	}
+
+	if envParams.mountPoint != "" {
+		cmd.Dir = envParams.mountPoint
+	}
+
+	if err := connection.SetupConnection(ctx, exec.Connections, cmd); err != nil {
+		return nil, ctx.Oops().Wrap(err)
+	}
+
+	envParams.cmd = cmd
+
+	return runCmd(ctx, envParams)
+}
+
+type commandContext struct {
+	cmd        *osExec.Cmd
+	artifacts  []Artifact
+	EnvVars    []types.EnvVar
+	extra      map[string]any
+	envs       []string
+	mountPoint string
+}
+
+func runCmd(ctx context.Context, cmd *commandContext) (*ExecDetails, error) {
+	var (
+		result ExecDetails
+		stdout bytes.Buffer
+		stderr bytes.Buffer
+	)
+
+	cmd.cmd.Stdout = &stdout
+	cmd.cmd.Stderr = &stderr
+
+	result.Error = cmd.cmd.Run()
+	result.Args = cmd.cmd.Args
+	result.Extra = cmd.extra
+	result.Path = cmd.cmd.Path
+	result.ExitCode = cmd.cmd.ProcessState.ExitCode()
+	result.Stderr = strings.TrimSpace(stderr.String())
+	result.Stdout = strings.TrimSpace(stdout.String())
+
+	ctx.Logger.V(3).Infof("%s exited with code=%d, stdout=%d bytes, stderr=%d bytes", cmd.cmd.Path, result.ExitCode, len(result.Stdout), len(result.Stderr))
+
+	for _, artifactConfig := range cmd.artifacts {
+		switch artifactConfig.Path {
+		case "/dev/stdout":
+			result.Artifacts = append(result.Artifacts, artifacts.Artifact{
+				Content: io.NopCloser(strings.NewReader(result.Stdout)),
+				Path:    "stdout",
+			})
+
+		case "/dev/stderr":
+			result.Artifacts = append(result.Artifacts, artifacts.Artifact{
+				Content: io.NopCloser(strings.NewReader(result.Stderr)),
+				Path:    "stderr",
+			})
+
+		default:
+			paths, err := fileUtils.UnfoldGlobs(artifactConfig.Path)
+			if err != nil {
+				return nil, err
+			}
+
+			for _, path := range paths {
+				file, err := os.Open(path)
+				if err != nil {
+					return nil, fmt.Errorf("error opening artifact file. path=%s; %w", path, err)
+				}
+
+				if stat, err := file.Stat(); err != nil {
+					return nil, fmt.Errorf("error getting artifact file stat. path=%s; %w", path, err)
+				} else if stat.IsDir() {
+					return nil, fmt.Errorf("artifact path (%s) is a directory. expected file", path)
+				}
+
+				result.Artifacts = append(result.Artifacts, artifacts.Artifact{
+					Content: file,
+					Path:    path,
+				})
+			}
+		}
+	}
+	if result.ExitCode != 0 {
+		return &result, ctx.Oops().With(
+			"cmd", cmd.cmd.Path,
+			"args", cmd.cmd.Args,
+			"error", result.Error.Error(),
+			"stderr", result.Stderr,
+			"stdout", result.Stdout,
+			"extra", result.Extra,
+			"exit-code", result.ExitCode,
+		).Code(fmt.Sprintf("exited with %d", result.ExitCode)).Errorf(result.Error.Error())
+	}
+
+	return &result, nil
+}
+
+func prepareEnvironment(ctx context.Context, exec Exec) (*commandContext, error) {
+	result := commandContext{
+		extra: make(map[string]any),
+	}
+
+	for _, env := range exec.EnvVars {
+		val, err := ctx.GetEnvValueFromCache(env, ctx.GetNamespace())
+		if err != nil {
+			return nil, fmt.Errorf("error fetching env value (name=%s): %w", env.Name, err)
+		}
+
+		result.envs = append(result.envs, fmt.Sprintf("%s=%s", env.Name, val))
+	}
+
+	if exec.Checkout != nil {
+		checkout := *exec.Checkout
+
+		if err := checkout.HydrateConnection(ctx); err != nil {
+			return nil, fmt.Errorf("error hydrating connection: %w", err)
+		}
+
+		result.mountPoint = lo.FromPtr(checkout.Destination)
+		if result.mountPoint == "" {
+			result.mountPoint = filepath.Join(os.TempDir(), "exec-checkout", hash.Sha256Hex(checkout.URL))
+		}
+		// We allow multiple checks to use the same checkout location, for disk space and performance reasons
+		// however git does not allow multiple operations to be performed, so we need to lock it
+		lock := checkoutLocks.TryLock(result.mountPoint, 5*time.Minute)
+		if lock == nil {
+			return nil, fmt.Errorf("failed to acquire checkout lock for %s", result.mountPoint)
+		}
+		defer lock.Release()
+
+		client, err := connection.CreateGitConfig(ctx, &checkout)
+		if err != nil {
+			return nil, err
+		}
+
+		if extra, err := client.Clone(ctx, result.mountPoint); err != nil {
+			return nil, err
+		} else {
+			for k, v := range extra {
+				result.extra[k] = v
+			}
+		}
+	}
+
+	return &result, nil
+}
