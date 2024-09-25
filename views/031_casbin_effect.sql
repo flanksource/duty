5d64f2147c59360171d7fb624e6926688909116f
Aditya Thebe <contact@adityathebe.com>
2024-09-25 20:45:10 +0545

    feat: casbin policy schema change

--- views/031_casbin_effect.sql
+++ views/031_casbin_effect.sql
@@ -1,1 +1,3 @@
-UPDATE casbin_rule SET v3 = 'allow' WHERE ptype = 'p' and (v3 is null or v3 = '')
+UPDATE casbin_rule SET v3 = 'allow' WHERE ptype = 'p' and (v3 is null or v3 = '');
+
+UPDATE casbin_rule SET v4 = 'true' WHERE ptype = 'p' and (v4 is null OR v4 = '');
