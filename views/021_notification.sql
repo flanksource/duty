fb8e0e5467f74800ef5fee5c0c8cd0610d5873ae
Aditya Thebe <contact@adityathebe.com>
2024-09-30 13:21:00 +0545

    feat: table activity trigger for permissions

--- views/021_notification.sql
+++ views/021_notification.sql
@@ -1,22 +1,3 @@
--- Notify on any updates/deletes on the notifications table
-CREATE OR REPLACE FUNCTION handle_notifications_updates_deletes()
-RETURNS TRIGGER AS $$
-BEGIN
-  IF TG_OP = 'DELETE' THEN
-    PERFORM pg_notify('table_activity', TG_TABLE_NAME || ' ' || OLD.id);
-  ELSE
-    PERFORM pg_notify('table_activity', TG_TABLE_NAME || ' ' || NEW.id);
-  END IF;
-
-  RETURN NULL;
-END
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE TRIGGER handle_notifications_updates_deletes_trigger
-AFTER UPDATE OR DELETE ON notifications
-FOR EACH ROW
-EXECUTE PROCEDURE handle_notifications_updates_deletes();
-
 -- Handle before updates for notifications
 CREATE OR REPLACE FUNCTION reset_notification_error_before_update()
 RETURNS TRIGGER AS $$
---
+++ views/033_table_activity.sql
@@ -0,0 +1,34 @@
+-- Cleanup previous trigger and function
+DROP TRIGGER IF EXISTS handle_notifications_updates_deletes_trigger ON notifications;
+
+DROP FUNCTION IF EXISTS handle_notifications_updates_deletes;
+
+-- Notify on any updates/deletes on these tables
+CREATE OR REPLACE FUNCTION notify_table_updates_and_deletes()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF TG_OP = 'DELETE' THEN
+    PERFORM pg_notify('table_activity', TG_TABLE_NAME || ' ' || OLD.id);
+  ELSE
+    PERFORM pg_notify('table_activity', TG_TABLE_NAME || ' ' || NEW.id);
+  END IF;
+
+  RETURN NULL;
+END
+$$ LANGUAGE plpgsql;
+
+DO $$ 
+DECLARE 
+  table_name TEXT;
+BEGIN 
+  FOR table_name IN SELECT unnest(ARRAY['notifications', 'permissions', 'teams'])
+  LOOP 
+    EXECUTE format('
+      CREATE OR REPLACE TRIGGER notify_updates_and_deletes
+      AFTER UPDATE OR DELETE ON %I
+      FOR EACH ROW
+      EXECUTE PROCEDURE notify_table_updates_and_deletes()',
+      table_name
+    );
+  END LOOP; 
+END $$;
