d3b75a3c9c144790c19a276eba6b7dd78c3fc2ca
Aditya Thebe <contact@adityathebe.com>
2024-11-29 15:18:14 +0545

    feat: scrape plugins

---
+++ models/scrape_plugin.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+
+	"github.com/flanksource/duty/types"
+	"github.com/google/uuid"
+)
+
+type ScrapePlugin struct {
+	ID        uuid.UUID  `json:"id"`
+	Name      string     `json:"name"`
+	Namespace string     `json:"namespace"`
+	Spec      types.JSON `json:"spec,omitempty"`
+	Source    string     `json:"source,omitempty"`
+	CreatedBy *uuid.UUID `json:"created_by,omitempty"`
+	CreatedAt time.Time  `json:"created_at" gorm:"<-:create"`
+	UpdatedAt *time.Time `json:"updated_at" gorm:"autoUpdateTime:false"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
