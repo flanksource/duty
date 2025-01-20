d81df4a97266c6822bf67199d30a3667f09b98b0
Aditya Thebe <contact@adityathebe.com>
2025-01-20 15:45:00 +0545

    fix: dot import
    * remove deprecated func

--- check_summary.go
+++ check_summary.go
@@ -4,15 +4,9 @@ import (
 	"github.com/flanksource/duty/context"
 	"github.com/flanksource/duty/models"
 	"github.com/flanksource/duty/query"
-	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // deprecated use query.CheckSummaryByID
 func CheckSummary(ctx context.Context, checkID string) (*models.CheckSummary, error) {
 	return query.CheckSummaryByID(ctx, checkID)
-}
-
-// deprecated use query.CheckSummary
-func QueryCheckSummary(ctx context.Context, dbpool *pgxpool.Pool, opts ...query.CheckSummaryOptions) ([]models.CheckSummary, error) {
-	return query.CheckSummary(ctx, opts...)
 }
