da73584c7a2d795e93e32774c5be343d89b39db3
Aditya Thebe <contact@adityathebe.com>
2025-01-23 14:22:10 +0545

    feat: Sensitive type (#1284)

--- secret/secret.go
+++ secret/secret.go
@@ -3,48 +3,33 @@ package secret
 import (
 	"fmt"
 
-	"github.com/flanksource/duty/connection"
 	"github.com/flanksource/duty/context"
-	"github.com/flanksource/duty/models"
-	"github.com/samber/lo"
-	"gocloud.dev/secrets"
 )
 
-var allowedConnectionTypes = []string{
-	models.ConnectionTypeAWSKMS,
-	models.ConnectionTypeGCPKMS,
-	models.ConnectionTypeAzureKeyVault,
-	// Vault not supported yet
+func Encrypt(ctx context.Context, sensitive Sensitive) (Ciphertext, error) {
+	keeper, err := createOrGetKeeper(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get secret keeper from connection (%s): %w", KMSConnection, err)
+	}
+
+	ciphertext, err := keeper.Encrypt(ctx, []byte(sensitive.PlainText()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to encrypt secret: %w", err)
+	}
+
+	return ciphertext, nil
 }
 
-func KeeperFromConnection(ctx context.Context, connectionString string) (*secrets.Keeper, error) {
-	conn, err := ctx.HydrateConnectionByURL(connectionString)
+func Decrypt(ctx context.Context, ciphertext Ciphertext) (Sensitive, error) {
+	keeper, err := createOrGetKeeper(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("failed to hydrate connection: %w", err)
-	} else if conn == nil {
-		return nil, fmt.Errorf("connection not found: %s", connectionString)
+		return nil, fmt.Errorf("failed to get secret keeper from connection (%s): %w", KMSConnection, err)
 	}
 
-	if !lo.Contains(allowedConnectionTypes, conn.Type) {
-		return nil, fmt.Errorf("connection type %s cannot be used to create a SecretKeeper", conn.Type)
+	decrypted, err := keeper.Decrypt(ctx, ciphertext)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decrypt secret: %w", err)
 	}
 
-	switch conn.Type {
-	case models.ConnectionTypeAWSKMS:
-		var kmsConn connection.AWSKMS
-		kmsConn.FromModel(*conn)
-		return kmsConn.SecretKeeper(ctx)
-
-	case models.ConnectionTypeAzureKeyVault:
-		var keyvaultConn connection.AzureKeyVault
-		keyvaultConn.FromModel(*conn)
-		return keyvaultConn.SecretKeeper(ctx)
-
-	case models.ConnectionTypeGCPKMS:
-		var kmsConn connection.GCPKMS
-		kmsConn.FromModel(*conn)
-		return kmsConn.SecretKeeper(ctx)
-	}
-
-	return nil, nil
+	return Sensitive(decrypted), nil
 }
