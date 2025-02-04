443675306ef99cd60e4c6bbc8c3d217e7131839b
Aditya Thebe <contact@adityathebe.com>
2025-02-04 18:10:48 +0545

    feat: match Query cel function

---
+++ query/template.go
@@ -0,0 +1,111 @@
+package query
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/flanksource/gomplate/v3/conv"
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/common/types"
+	"github.com/google/cel-go/common/types/ref"
+
+	"github.com/flanksource/duty/context"
+	"github.com/flanksource/duty/models"
+	dutyTypes "github.com/flanksource/duty/types"
+)
+
+func init() {
+	context.CelEnvFuncs["matchQueryCel"] = MatchQueryCelFunc
+	context.TemplateFuncs["matchQueryTemplate"] = MatchQueryGotemplateFunc
+}
+
+func MatchQueryGotemplateFunc(ctx context.Context) any {
+	return matchQuery
+}
+
+func MatchQueryCelFunc(ctx context.Context) cel.EnvOption {
+	return cel.Function("matchQuery",
+		cel.Overload("matchQuery",
+			[]*cel.Type{cel.MapType(cel.StringType, cel.DynType), cel.StringType},
+			cel.BoolType,
+			cel.FunctionBinding(func(args ...ref.Val) ref.Val {
+				peg := conv.ToString(args[1])
+
+				resourceSelectableRaw, err := convertMap(args[0])
+				if err != nil {
+					return types.WrapErr(errors.New("matchQuery expects the first argument to be a map[string]any"))
+				}
+
+				match, err := matchQuery(resourceSelectableRaw, peg)
+				if err != nil {
+					return types.WrapErr(err)
+				}
+
+				return types.Bool(match)
+			}),
+		),
+	)
+}
+
+func matchQuery(resourceSelectableRaw map[string]any, peg string) (bool, error) {
+	var resourceSelectable dutyTypes.ResourceSelectable
+
+	// NOTE: We check for fields in the map to determine what resource to unmarshal to.
+
+	if _, ok := resourceSelectableRaw["config_class"]; ok {
+		var config models.ConfigItem
+		if b, err := json.Marshal(resourceSelectableRaw); err != nil {
+			return false, err
+		} else if err := json.Unmarshal(b, &config); err != nil {
+			return false, err
+		}
+
+		resourceSelectable = config
+	} else if _, ok := resourceSelectableRaw["category"]; ok {
+		var playbook models.Playbook
+		if b, err := json.Marshal(resourceSelectableRaw); err != nil {
+			return false, err
+		} else if err := json.Unmarshal(b, &playbook); err != nil {
+			return false, err
+		}
+
+		resourceSelectable = &playbook
+	} else if _, ok := resourceSelectableRaw["topology_id"]; ok {
+		var component models.Component
+		if b, err := json.Marshal(resourceSelectableRaw); err != nil {
+			return false, err
+		} else if err := json.Unmarshal(b, &component); err != nil {
+			return false, err
+		}
+
+		resourceSelectable = component
+	} else if _, ok := resourceSelectableRaw["canary_id"]; ok {
+		var check models.Check
+		if b, err := json.Marshal(resourceSelectableRaw); err != nil {
+			return false, err
+		} else if err := json.Unmarshal(b, &check); err != nil {
+			return false, err
+		}
+
+		resourceSelectable = check
+	}
+
+	rs := dutyTypes.ResourceSelector{Search: peg}
+	return rs.Matches(resourceSelectable), nil
+}
+
+func convertMap(arg ref.Val) (map[string]any, error) {
+	switch m := arg.Value().(type) {
+	case map[ref.Val]ref.Val:
+		var out = make(map[string]any)
+		for key, val := range m {
+			out[key.Value().(string)] = val.Value()
+		}
+		return out, nil
+	case map[string]any:
+		return m, nil
+	default:
+		return nil, fmt.Errorf("not a map %T", arg.Value())
+	}
+}
---
+++ query/template_test.go
@@ -0,0 +1,70 @@
+package query_test
+
+import (
+	"testing"
+
+	"github.com/flanksource/duty/context"
+	"github.com/flanksource/duty/models"
+	"github.com/flanksource/gomplate/v3"
+	"github.com/google/uuid"
+	"github.com/samber/lo"
+	"gotest.tools/v3/assert"
+)
+
+func TestMatchQuery(t *testing.T) {
+	config := models.ConfigItem{
+		Name: lo.ToPtr("aws-demo"),
+	}
+
+	playbook := models.Playbook{
+		Name:     "airsonic",
+		Category: "kubernetes",
+	}
+
+	check := models.Check{
+		Name:     "webhook",
+		CanaryID: uuid.New(),
+	}
+
+	component := models.Component{
+		Name:       "azure-demo",
+		AgentID:    uuid.New(),
+		TopologyID: lo.ToPtr(uuid.New()),
+		Namespace:  "mission-control",
+	}
+
+	runTests(t, []TestCase{
+		{map[string]any{"config": config.AsMap()}, "matchQuery(config, 'name=aws*')", "true"},
+		{map[string]any{"config": config.AsMap()}, "matchQuery(config, 'name=azure*')", "false"},
+
+		{map[string]any{"playbook": playbook.AsMap()}, "matchQuery(playbook, 'name=air*')", "true"},
+		{map[string]any{"playbook": playbook.AsMap()}, "matchQuery(playbook, 'name=azure*')", "false"},
+
+		{map[string]any{"check": check.AsMap()}, "matchQuery(check, 'name=web*')", "true"},
+		{map[string]any{"check": check.AsMap()}, "matchQuery(check, 'name=azure*')", "false"},
+
+		{map[string]any{"component": component.AsMap()}, "matchQuery(component, 'name=azure*')", "true"},
+		{map[string]any{"component": component.AsMap()}, "matchQuery(component, 'namespace=*control')", "true"},
+		{map[string]any{"component": component.AsMap()}, "matchQuery(component, 'namespace=default')", "false"},
+	})
+}
+
+type TestCase struct {
+	env        map[string]interface{}
+	expression string
+	out        string
+}
+
+func runTests(t *testing.T, tests []TestCase) {
+	ctx := context.New()
+	for _, tc := range tests {
+		t.Run(tc.expression, func(t *testing.T) {
+			out, err := ctx.RunTemplate(gomplate.Template{
+				Expression: tc.expression,
+			}, tc.env)
+
+			assert.ErrorIs(t, nil, err)
+			assert.Equal(t, tc.out, out)
+		})
+	}
+}
