4694ea83cde301f4ce2cc42d5faa340e9bc3f3bc
Aditya Thebe <contact@adityathebe.com>
2025-05-31 18:19:54 +0545

    fix: migration test for postgres v14 (#1448)
    migrate would never return exit code 1 so the CI would always pass even
    if the migration failed.

--- hack/migrate/main.go
+++ hack/migrate/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/flanksource/commons/logger"
 	"github.com/flanksource/duty"
 	"github.com/flanksource/duty/api"
@@ -8,24 +10,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	disableRLS = os.Getenv("DUTY_DB_DISABLE_RLS") == "true"
+	connection string
+)
+
 var migrate = &cobra.Command{
 	Use: "migrate",
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := duty.Migrate(api.Config{
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return duty.Migrate(api.Config{
 			ConnectionString: connection,
-			EnableRLS:        true,
+			EnableRLS:        !disableRLS,
 			Postgrest:        api.DefaultConfig.Postgrest,
-		}); err != nil {
-			logger.Fatalf(err.Error())
-		}
+		})
 	},
 }
-
-var connection string
 
 func main() {
 	migrate.Flags().StringVar(&connection, "db-url", "", "Database URI: scheme://user:pass@host:port/database")
 	if err := migrate.Execute(); err != nil {
-		logger.Fatalf("failed to run migration: %v", err)
+		logger.Errorf("failed to run migration: %v", err)
+		os.Exit(1)
 	}
 }
