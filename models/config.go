fe39c4dfe8aa96d12afa4f7a932581ac6122bc3e
Aditya Thebe <contact@adityathebe.com>
2025-06-20 17:35:57 +0545

    feat: add View model for storing Kubernetes custom resources

--- models/config.go
+++ models/config.go
@@ -631,6 +631,35 @@ func (a ConfigAnalysis) String() string {
 	return fmt.Sprintf("[%s/%s] %s", a.ConfigType, a.ExternalID, a.Analyzer)
 }
 
+// View represents the views database table
+type View struct {
+	ID        uuid.UUID  `json:"id" gorm:"default:generate_ulid()"`
+	Name      string     `json:"name"`
+	Namespace string     `json:"namespace"`
+	Spec      types.JSON `json:"spec"`
+	Source    string     `json:"source" gorm:"default:KubernetesCRD"`
+	CreatedBy *uuid.UUID `json:"created_by,omitempty"`
+	CreatedAt time.Time  `json:"created_at" gorm:"<-:create"`
+	UpdatedAt *time.Time `json:"updated_at" gorm:"autoUpdateTime:false"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
+
+func (v View) PK() string {
+	return v.ID.String()
+}
+
+func (View) TableName() string {
+	return "views"
+}
+
+func (v View) AsMap(removeFields ...string) map[string]any {
+	return asMap(v, removeFields...)
+}
+
+func (v View) GetNamespace() string {
+	return v.Namespace
+}
+
 type ExternalID struct {
 	ConfigType string
 	ExternalID []string
