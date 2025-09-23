d396abf2f8be13d7cfe71c8ab7808fe86dc65b58
Aditya Thebe <contact@adityathebe.com>
2025-09-23 11:40:24 +0545

    feat: View labels

--- models/view.go
+++ models/view.go
@@ -15,16 +15,17 @@ import (
 
 // View represents the views database table
 type View struct {
-	ID        uuid.UUID  `json:"id" gorm:"default:generate_ulid()"`
-	Name      string     `json:"name"`
-	Namespace string     `json:"namespace" gorm:"default:NULL"`
-	Spec      types.JSON `json:"spec"`
-	Source    string     `json:"source" gorm:"default:KubernetesCRD"`
-	CreatedBy *uuid.UUID `json:"created_by,omitempty"`
-	CreatedAt time.Time  `json:"created_at" gorm:"<-:create"`
-	UpdatedAt *time.Time `json:"updated_at" gorm:"autoUpdateTime:false"`
-	Error     *string    `json:"error,omitempty" gorm:"default:NULL"`
-	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+	ID        uuid.UUID           `json:"id" gorm:"default:generate_ulid()"`
+	Name      string              `json:"name"`
+	Namespace string              `json:"namespace" gorm:"default:NULL"`
+	Spec      types.JSON          `json:"spec"`
+	Source    string              `json:"source" gorm:"default:KubernetesCRD"`
+	Labels    types.JSONStringMap `json:"labels,omitempty" gorm:"type:jsonb"`
+	CreatedBy *uuid.UUID          `json:"created_by,omitempty"`
+	CreatedAt time.Time           `json:"created_at" gorm:"<-:create"`
+	UpdatedAt *time.Time          `json:"updated_at" gorm:"autoUpdateTime:false"`
+	Error     *string             `json:"error,omitempty" gorm:"default:NULL"`
+	DeletedAt *time.Time          `json:"deleted_at,omitempty"`
 }
 
 func (v View) GeneratedTableName() string {
