05d211d8ea1e006994c4ff0aeb64e89652942662
Aditya Thebe <contact@adityathebe.com>
2025-09-30 12:06:30 +0545

    chore(topology): Bring in NewTopologyParams from Canary Checker

--- query/topology_options.go
+++ query/topology_options.go
@@ -2,8 +2,13 @@ package query
 
 import (
 	"encoding/json"
+	"net/url"
+	"strconv"
+	"strings"
 
+	"github.com/flanksource/commons/collections"
 	"github.com/flanksource/commons/hash"
+
 	"github.com/flanksource/duty/models"
 )
 
@@ -47,6 +52,41 @@ func (opt TopologyOptions) CacheKey() string {
 	opt.Status = []string{}
 	data, _ := json.Marshal(opt)
 	return hash.Sha256Hex(string(data))
+}
+
+func NewTopologyParams(values url.Values) TopologyOptions {
+	parseItems := func(items string) []string {
+		if strings.TrimSpace(items) == "" {
+			return nil
+		}
+		return strings.Split(strings.TrimSpace(items), ",")
+	}
+
+	var labels map[string]string
+	if values.Get("labels") != "" {
+		labels = collections.KeyValueSliceToMap(strings.Split(values.Get("labels"), ","))
+	}
+
+	var err error
+	var depth = DefaultDepth
+	if depthStr := values.Get("depth"); depthStr != "" {
+		depth, err = strconv.Atoi(depthStr)
+		if err != nil {
+			depth = DefaultDepth
+		}
+	}
+	return TopologyOptions{
+		ID:        values.Get("id"),
+		Owner:     values.Get("owner"),
+		Labels:    labels,
+		Status:    parseItems(values.Get("status")),
+		Depth:     depth,
+		Types:     parseItems(values.Get("type")),
+		Flatten:   values.Get("flatten") == "true",
+		SortBy:    TopologyQuerySortBy(values.Get("sortBy")),
+		SortOrder: values.Get("sortOrder"),
+		NoCache:   values.Has("noCache") || values.Has("no-cache"),
+	}
 }
 
 // Map of tag keys to the list of available values
