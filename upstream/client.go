8b56ccc1aa848bcc8c6792cd69d8934969b2fe7f
Yash Mehrotra <yashmehrotra95@gmail.com>
2025-10-30 15:10:30 +0530

    chore: fix non 2xx error handling

--- upstream/client.go
+++ upstream/client.go
@@ -116,11 +116,16 @@ func (t *UpstreamClient) push(ctx context.Context, method string, msg *PushData)
 		respBody, _ := io.ReadAll(resp.Body)
 
 		var httpErr api.HTTPError
-		if json.Unmarshal(respBody, &httpErr) == nil {
-			return fmt.Errorf("non 2xx response: %w", httpErr)
+		err := json.Unmarshal(respBody, &httpErr)
+		if err == nil {
+			if httpErr.Err == ForeignKeyError {
+				return &httpErr
+			} else {
+				return fmt.Errorf("non 2xx response: %w", httpErr)
+			}
+		} else {
+			return fmt.Errorf("upstream server returned error status[%d]: %s", resp.StatusCode, parseResponse(string(respBody)))
 		}
-
-		return fmt.Errorf("upstream server returned error status[%d]: %s", resp.StatusCode, parseResponse(string(respBody)))
 	}
 	histogram.Label(StatusLabel, StatusOK).Since(start)
 	return nil
