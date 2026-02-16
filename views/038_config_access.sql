92eb3b0c7ecea2104315c1078bec3d8dfbf32074
Yash Mehrotra <yashmehrotra95@gmail.com>
2026-02-17 02:08:20 +0530

    feat: add user type in config access summary

--- views/038_config_access.sql
+++ views/038_config_access.sql
@@ -5,7 +5,7 @@ DROP VIEW IF EXISTS user_config_access_summary;
 
 -- config_access_unwrapped
 -- flattens config access permissions by expanding group memberships
-CREATE OR REPLACE VIEW config_access_unwrapped AS 
+CREATE OR REPLACE VIEW config_access_unwrapped AS
 SELECT
   generate_ulid()::TEXT as id,
   config_access.config_id,
@@ -45,7 +45,7 @@ SELECT
 DROP VIEW IF EXISTS config_access_summary;
 
 CREATE VIEW config_access_summary AS
-SELECT 
+SELECT
   config_items.id as config_id,
   config_items.name as config_name,
   config_items.type as config_type,
@@ -54,6 +54,7 @@ SELECT
   external_roles.name as "role",
   external_users.name as "user",
   external_users.email as "email",
+  external_users.user_type as user_type,
   config_access_unwrapped.created_at as created_at,
   config_access_unwrapped.deleted_at as deleted_at,
   config_access_unwrapped.created_by as created_by,
@@ -64,7 +65,7 @@ FROM config_access_unwrapped
 JOIN config_items ON config_access_unwrapped.config_id = config_items.id
 JOIN external_users ON config_access_unwrapped.external_user_id = external_users.id
 LEFT JOIN external_roles ON config_access_unwrapped.external_role_id = external_roles.id
-LEFT JOIN config_access_logs 
+LEFT JOIN config_access_logs
   ON config_access_unwrapped.config_id = config_access_logs.config_id AND
   config_access_unwrapped.external_user_id = config_access_logs.external_user_id
 WHERE config_access_unwrapped.deleted_at IS NULL;
