68d434df2c67757fb16c9d8c7c833e65a5b0dfc2
Yash Mehrotra <yashmehrotra95@gmail.com>
2026-04-20 14:54:56 +0530

    Revert "chore: fix tests"
    This reverts commit ce357034a007bbdc7c0dae6a4703a062d302adc8.

--- tests/config_type_summary_test.go
+++ tests/config_type_summary_test.go
@@ -233,8 +233,8 @@ var _ = ginkgo.Describe("Check config_class_summary view", ginkgo.Ordered, func(
 		Expect(err).To(BeNil())
 
 		expectedTypeSummary10d := []summaryRow{
-			{Type: "Test::type-A", Count: 3, Changes: 6, Health: map[string]any{"healthy": float64(2), "unhealthy": float64(1)}, Analysis: map[string]any{"availability": float64(2), "cost": float64(2), "security": float64(2)}},
-			{Type: "Test::type-B", Count: 2, Changes: 6, Health: map[string]any{"healthy": float64(2)}, Analysis: map[string]any{"availability": float64(1), "cost": float64(1), "security": float64(2)}},
+			{Type: "Test::type-A", Count: 3, Changes: 8, Health: map[string]any{"healthy": float64(2), "unhealthy": float64(1)}, Analysis: map[string]any{"availability": float64(2), "cost": float64(2), "security": float64(2)}},
+			{Type: "Test::type-B", Count: 2, Changes: 8, Health: map[string]any{"healthy": float64(2)}, Analysis: map[string]any{"availability": float64(1), "cost": float64(1), "security": float64(2)}},
 			{Type: "Test::type-C", Count: 1, Changes: 1, Health: map[string]any{"unhealthy": float64(1)}, Analysis: nil},
 		}
 
